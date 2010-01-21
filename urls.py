0b28ce796359c8d0206d26ab4e62be8dde194593
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2010-01-21 11:09:49 -0500

    threaded discussions- adding django app

--- urls.py
+++ urls.py
@@ -21,10 +21,6 @@ redirect_after_logout = getattr(settings, 'LOGOUT_REDIRECT_URL', None)
 urlpatterns = patterns('',
                        (r'^comments/', include('django.contrib.comments.urls')),
                        
-                       
-                       #this is apparently not used any more:
-                       #(r'^threadedcomments/', include('threadedcomments.urls')),
-
                        (r'^accounts/logout/$','django.contrib.auth.views.logout', 
                         {'next_page': redirect_after_logout}),
                        login_page,#see above
@@ -43,7 +39,7 @@ urlpatterns = patterns('',
 
                        (r'^asset/',include('mondrian.assetmgr.urls')),
                        (r'^yourspace/',include('mondrian.mondrian_main.urls')),
-
+                       
                        #redundant, but for published projects/legacy
                        #(r'^project/',include('mondrian.projects.urls')),
                        #override/shortcut for published projects
@@ -52,6 +48,9 @@ urlpatterns = patterns('',
                            name='project-view'
                            ),
                        (r'^explore/$','assetmgr.views.archive_explore'),
+
+                       #threaded discussion:
+                       (r'^discussion/',include('mondrian.discussions.urls')),
 
                        ### Public Access ###
                        (r'', include(structuredcollaboration.urls)), #import at root

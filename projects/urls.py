79021125ebe96e26029a7888c730bf0512e6e08d
Schuyler Duveen <sky@skyb.us>
2010-02-19 16:10:11 -0500

    tweak urls so we can put arbitrary stuff at end of project urls to avoid cache poisoning GoogleChrome with json data instead of html page

--- projects/urls.py
+++ projects/urls.py
@@ -6,7 +6,9 @@ urlpatterns = patterns(
 
     #narrowly overrides the username space
     #when it's just numbers
-    url(r'^(?P<project_id>\d+)/$',
+    #[^v/]* is so we can do a json query with a different url
+    #to avoid Chrome bug of replacing cache with 
+    url(r'^(?P<project_id>\d+)/[^v/]*$',
         'project_readonly_view',
         name='project-view'),
 
@@ -14,17 +16,16 @@ urlpatterns = patterns(
         'your_projects',
         name='your-space-projects',
         ),
-        
-    url(r'^project/(?P<project_id>\d+)/$',
-        'view_project',
-        name='project-workspace',
-        ),
 
-    url(r'^project/(?P<project_id>\d+)/version/(?P<version_number>\d+)/$',
+    url(r'^project/(?P<project_id>\d+)/version/(?P<version_number>\d+)/[^v/]*$',
         'project_version_preview',
         name='project_version_preview',
         ),
-    url(r'^projectversion/(?P<projectversion_id>\d+)/$',
+    url(r'^project/(?P<project_id>\d+)/[^v/]*$',
+        'view_project',
+        name='project-workspace',
+        ),
+    url(r'^projectversion/(?P<projectversion_id>\d+)/[^v/]*$',
         'project_version_view',
         name='projectversion-view',
         ),

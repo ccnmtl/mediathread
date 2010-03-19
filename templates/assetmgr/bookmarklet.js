d9f2b2fdedf493fae72787a5e55fcd20250b823a
Schuyler Duveen <sky@skyb.us>
2010-03-19 18:22:31 -0400

    new /explore/ page and some small fixes

---
+++ templates/assetmgr/bookmarklet.js
@@ -0,0 +1,26 @@
+javascript:/*SHERD_UNIVERSAL_BOOKMARKLET____________________*/(function(host,bookmarklet_url){ 
+
+var b=document.body;
+window.SherdBookmarkletOptions={mondrian_url:'http://'+host+'/save/?',action:'jump'};
+var t='text/javascript';
+if(b){
+    z=document.createElement('script');
+    z.type=t;
+    z.src='http://'+host+bookmarklet_url;
+    b.appendChild(z);
+    if (typeof jQuery=='undefined') {
+        y=document.createElement('script');
+        y.type=t;
+        y.src='http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js';
+        var onload = (/Trident/.test(navigator.userAgent))?'onreadystatechange':'onload';
+        y[onload]=function(){
+            window.SherdBookmarkletOptions.jQuery = jQuery.noConflict(true);
+            if (MondrianBookmarklet && MondrianBookmarklet.onJQuery) {
+                MondrianBookmarklet.onJQuery(window.SherdBookmarkletOptions.jQuery);
+            }
+        };
+        b.appendChild(y);
+    }
+}
+
+ })('{{request.get_host}}','{%url analyze-bookmarklet "analyze.js" %}')

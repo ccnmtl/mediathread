e81170dee5c1a6f59736571c04b1537de6b58705
Schuyler Duveen <sky@skyb.us>
2010-05-14 17:41:13 -0400

    mochi to jquery in discussion.js

--- media/js/discussion.js
+++ media/js/discussion.js
@@ -1,15 +1,15 @@
 addLoadEvent(function discussion_init() {
     var next_response_loc = false;
-    var frm = $('comment-form');
+    var frm = jQuery('#comment-form').get(0);
 
     function open_comment_form(evt) {
-        var respond = evt.src();
+        var respond = evt.target;
         frm.elements['parent'].value = respond.getAttribute("data-comment");
         if (!next_response_loc) {
 	    next_response_loc = respond;
             respond.nextSibling.appendChild(frm);
-            showElement(frm);
-            $('id_comment').focus();
+            jQuery(frm).show();
+            jQuery('#id_comment').focus();
             tinyMCE.execCommand("mceAddControl", false, "id_comment");
             } else {
 		if (next_response_loc == respond) {
@@ -28,9 +28,7 @@ addLoadEvent(function discussion_init() {
         tinyMCE.execCommand("mceRemoveControl", false, "id_comment");
     }
 
-    forEach($$('.respond_prompt'), function(elt) {
-        connect(elt,'onclick', open_comment_form);
-    });
+    jQuery('.respond_prompt').click(open_comment_form);
 
     tinyMCE.onRemoveEditor.add(function(manager, ed) {
         //logDebug("third focus");
@@ -38,7 +36,7 @@ addLoadEvent(function discussion_init() {
             next_response_loc.nextSibling.appendChild(document.forms[0]);
             tinyMCE.execCommand("mceAddControl", false, "id_comment");
 	} else {
-            hideElement('comment-form');
+            jQuery('#comment-form').hide();
 	}
     });
     tinyMCE.onAddEditor.add(function(manager, ed) {
@@ -57,43 +55,44 @@ addLoadEvent(function discussion_init() {
 
 function AjaxComment(form) {
     this.form = form;
-    this.username = $('logged_in_name').textContent;
+    this.username = jQuery('#logged_in_name').text();
 
-    connect(form,'onsubmit',this, 'submit');
-    
+    jQuery(form).bind('submit',this,this.submit);
 }
 
 AjaxComment.prototype.submit = function(evt) {
+    var self = evt.data;
     tinyMCE.triggerSave();
     evt.preventDefault();
-    var form_val_array = formContents(this.form);
 
-    var def = doXHR(this.form.action,
-        {method:'POST',
-        sendContent:queryString(form_val_array),
-        headers:[["Content-type","application/x-www-form-urlencoded"]]
-        }
-                   );
-    //frm.elements['timestamp'].value = '';
-    //frm.elements['security_hash'].value = '';
-    def.addBoth(bind(this.oncomplete,this,form_val_array),
-                this.onfail);
+    var frm = jQuery(self.form);
+    form_val_array = frm.serializeArray();
+
+    jQuery.ajax({
+        type: 'POST',
+        url: self.form.action,
+        data: form_val_array,//default will serialize?
+        dataType: 'html',
+        success: self.oncomplete,
+        error: self.onfail,
+        context: {'self':self,'form_val_array':form_val_array}
+    });
 }
-var sky;
-AjaxComment.prototype.oncomplete = function(form_val_array, func, xhr) {
-    var form_vals = {};
-    for (var i=0;i<form_val_array[0].length;i++) {
-        form_vals[form_val_array[0][i]] = form_val_array[1][i];
-    }
 
-    var res = this.parseResponse(xhr);
+AjaxComment.prototype.oncomplete = function(responseText, textStatus, xhr) {
+    var self = this.self;
+    var form_vals = {};
+    for (var i=0;i<this.form_val_array.length;i++) {
+        form_vals[this.form_val_array[i].name] = this.form_val_array[i].value;
+    }
+    var res = self.parseResponse(xhr);
     if (xhr.status ==200) {
         if (res.comment_id) {
             ///1. insert new comment into DOM
-            var html_comment = this.create(
+            var html_comment = self.create(
                 {'id':res.comment_id,
                  'comment':form_vals.comment,
-                 'name':this.username
+                 'name':self.username
                 });
             parent_html = $('comment-' +form_vals['parent']);
             var ul = document.createElement('ul');
@@ -101,11 +100,13 @@ AjaxComment.prototype.oncomplete = function(form_val_array, func, xhr) {
             parent_html.appendChild(ul);
             ul.innerHTML = html_comment.text;
             ///2. decorate citations and respond link
-            DjangoSherd_decorate_citations(
-                getElementsByTagAndClassName('a','materialCitation',ul));
+            DjangoSherd_decorate_citations( 
+                //passing to Mochi forEach so needs to be an array
+                jQuery('a.materialCitation',ul).toArray()
+            );
 
-            var respond_elt = getFirstElementByTagAndClassName('span','respond_prompt',ul);
-            connect(respond_elt, 'onclick', open_comment_form);
+            jQuery('span.respond_prompt',ul).click(open_comment_form);
+
             ///3. reset form and set new validation key
             
             frm.elements['comment'] ='';
@@ -120,9 +121,9 @@ AjaxComment.prototype.oncomplete = function(form_val_array, func, xhr) {
     }
 }
 
-AjaxComment.prototype.onfail = function(func, xhr) {
+AjaxComment.prototype.onfail = function(xhr, textStatus, errorThrown) {
     alert("There was an error submitting your comment!  Please report this to the system administrator.");
-    console.log('error');
+    console.log('error:'+errorThrown);
     console.log(xhr);    
 }
 

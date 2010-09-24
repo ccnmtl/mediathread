a58835e772f7232f14b9b83e00e283e53cb00b1c
Schuyler Duveen <sky@skyb.us>
2010-09-24 14:33:33 -0400

    slightly friendlier asset admin change form

--- assetmgr/admin.py
+++ assetmgr/admin.py
@@ -1,5 +1,12 @@
 from models import Asset, Source
 from django.contrib import admin
 
-admin.site.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Asset
+        
+    change_form_template="assetmgr/admin_change_form.html"
+
+admin.site.register(Asset,AssetAdmin)
 admin.site.register(Source)
+

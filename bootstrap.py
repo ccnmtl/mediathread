f8dffe120bf58d22e7af077a8dc5b911b1e4274d
Schuyler Duveen <sky@skyb.us>
2011-02-14 15:15:31 -0500

    make mediathread deployable on x86_64 2.6 server (bootstrap.py tweak and mx include for psycopg dependency)

--- bootstrap.py
+++ bootstrap.py
@@ -15,3 +15,7 @@ subprocess.call(["python",os.path.join(pwd,"pip.py"),"install",
                  "--enable-site-packages",
                  "--requirement",os.path.join(pwd,"requirements/apps.txt")])
 
+# do not want to try to install the egg on 2.5
+if sys.version.startswith("2.6"):
+    subprocess.call([os.path.join(vedir,"bin/easy_install"),
+                     os.path.join(pwd,"requirements/eggs/egenix_mx_base-3.1.3-py2.6-linux-x86_64.egg")])

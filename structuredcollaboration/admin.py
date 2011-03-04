b889473b397a6551db34a1c7724d5eb259ee21ec
Schuyler Duveen <sky@skyb.us>
2011-03-04 13:55:16 -0500

    course slug debugging

--- structuredcollaboration/admin.py
+++ structuredcollaboration/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.site.register(CollaborationPolicyRecord)
 
 class CollaborationAdmin(admin.ModelAdmin):
-    search_fields = ('title',)
-    list_display = ('title','group','user','_policy',)
+    search_fields = ('title','slug',)
+    list_display = ('title','group','user','_policy','slug',)
 
 admin.site.register(Collaboration, CollaborationAdmin)

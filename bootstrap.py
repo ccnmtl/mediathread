85cdccb7fda1ca10e6955d669cf5519053ce6016
Schuyler Duveen <sky@skyb.us>
2011-03-31 22:42:42 -0400

    bootstrap: allow other egenix-mx-base files in requirements/eggs/

--- bootstrap.py
+++ bootstrap.py
@@ -13,9 +13,12 @@ if os.path.exists(vedir):
 subprocess.call(["python",os.path.join(pwd,"pip.py"),"install",
                  "-E",os.path.join(pwd,"ve"),
                  "--enable-site-packages",
+                 "--index-url=''",
                  "--requirement",os.path.join(pwd,"requirements/apps.txt")])
 
 # do not want to try to install the egg on 2.5
 if sys.version.startswith("2.6"):
     subprocess.call([os.path.join(vedir,"bin/easy_install"),
-                     os.path.join(pwd,"requirements/eggs/egenix_mx_base-3.1.3-py2.6-linux-x86_64.egg")])
+                     '-f',os.path.join(pwd,"requirements/eggs/"),
+                     'egenix-mx-base',
+                     ])

e5b242a125a1b73e22fd2fec9e312ecd0eec42d3
sdreher <sdreher@gmail.com>
2011-12-11 14:33:33 -0500

    * Each panel gets a sidebar. I think this works better, and allows a straightforward implementation of show/hide on the panel.

--- media/js/panels.js
+++ media/js/panels.js
@@ -2,39 +2,55 @@
     
     window.PanelManager = new (function PanelManager(){
         var self = this;
+        var sliding_duration = 500;
         
         this.init = function() {
-            jQuery(".sliding-panel-closed").click(self.slidePanelClick);
+            jQuery(".sliding-panel-tab").click(self.slidePanelClick);
         }
         
         this.slidePanelClick = function(evt) {
             if (jQuery(this).hasClass("sliding-panel-open")) {
-                jQuery(this).toggleClass("sliding-panel-open sliding-panel-closed");
+                console.log("click on open");
+                //jQuery(this).toggleClass("sliding-panel-open sliding-panel-closed");
             } else {
                 // open me
-                var screenWidth = jQuery(document).width();
+                var panel = jQuery(this).next()[0];
                 
-                jQuery(this).toggleClass("sliding-panel-open sliding-panel-closed");
+                var screenWidth = jQuery(document).width();
+                console.log("Screen Width: " + screenWidth);
                 
                 var totalPanelWidth = 0;
-                jQuery(".sliding-panel-open").each(function() { totalPanelWidth += jQuery(this).width() });
+                var siblings = jQuery(panel).siblings(".sliding-panel-open, .sliding-panel-tab");
+                jQuery(siblings).each(function() { totalPanelWidth += jQuery(this).width() });
                 
-                if (totalPanelWidth > screenWidth) {
-                    var siblings = jQuery(this).siblings(".sliding-panel-open");
+                jQuery(panel).switchClass("sliding-panel-closed", "sliding-panel-open", self.sliding_duration);
+                
+                setTimeout(function() {
+                    totalPanelWidth += jQuery(panel).width();
+                    console.log("Total Panel Width: " + totalPanelWidth);
                     
-                    // close on the left or right?
-                    if (jQuery(this).prev(".sliding-panel-open").length > 0) {
-                        jQuery(siblings[0]).toggleClass("sliding-panel-open sliding-panel-closed");
+                    var prevPanels = jQuery(panel).prevAll(".sliding-panel-open");
+                    var nextPanels = jQuery(panel).nextAll(".sliding-panel-open");
                         
-                        // close on the left
-                    } else if (jQuery(this).next(".sliding-panel-open").length > 0) {
-                        // close on the right
-                        jQuery(siblings[siblings.length - 1]).toggleClass("sliding-panel-open sliding-panel-closed");
+                    if (totalPanelWidth > screenWidth) {
+                        // close on the left or right? limited to one close for the moment.
+                        var sib = null;
+                        if (prevPanels.length > 0) {
+                            // close on the left -- furthest out
+                            sib = prevPanels[prevPanels.length-1];
+                        } else if (nextPanels.length > 0) {
+                            // close on the right -- furthest out
+                            sib = nextPanels[nextPanels.length-1];
+                        }
+                            
+                        if (sib) {
+                            jQuery(sib).animate({width: '0px'}, self.sliding_duration, function() {
+                                jQuery(sib).toggleClass("sliding-panel-open sliding-panel-closed");
+                                jQuery(sib).removeAttr("style");
+                            });
+                        }
                     }
-                }
-                
-                jQuery(".sliding-panel").unbind("click");
-                jQuery(".sliding-panel-closed").bind("click", self.slidePanelClick);
+                }, self.sliding_duration);
             }
         }
     });

3a9ec8d258404840b0f39ba23b17b701cfae9b24
sdreher <sdreher@gmail.com>
2012-02-05 16:14:02 -0500

    sliding - automatically close when there's not enough screen real estate.

--- media/js/panel.js
+++ media/js/panel.js
@@ -3,57 +3,70 @@
         var self = this;
       
         this.init = function(config) {
-            jQuery(window).resize(function () {  
-                self.onResize();
-            });
-            
-            jQuery(".panhandle-stripe").click(function(event) {
-                self.onClickPanelLeftHandle(this, event);
-            });
+            self.config = config;
             
             jQuery(".pantab-container").click(function(event) {
-                self.onClickPanelRightHandle(this, event);
+                self.slidePanel(this, event);
             });
-
-            
-            // figure out which panels get shown/hidden
-            // config should contain the rules
-            // panels ordered by show/hide behavior?
         }
         
-        this.onClickPanelLeftHandle = function(element, event) {
-            // Open the panhandle's panel
-            var panel = jQuery(element).nextAll("td.panel-container");
-            jQuery(panel[0]).toggleClass("open closed");
-            
-            var panelTabContainer = jQuery(panel).nextAll("td.pantab-container");
-            var panelTab = jQuery(panelTabContainer[0]).children("div.pantab");
-            jQuery(panelTab[0]).toggleClass("open closed");
-            
-            var screenWidth = jQuery(window).width();
-            console.log("The screen is: " + screenWidth);
-            jQuery("table.panel").css("width", screenWidth);
-        }
-        
-        this.onClickPanelRightHandle = function(element, event) {
+        this.slidePanel = function(pantab_container, event) {
             // Open/close this panhandle's panel
-            var panel = jQuery(element).prevAll("td.panel-container");
+            var panel = jQuery(pantab_container).prevAll("td.panel-container");
             jQuery(panel[0]).toggleClass("open closed");
             
-            var panelTab = jQuery(element).children("div.pantab");
+            var panelTab = jQuery(pantab_container).children("div.pantab");
             jQuery(panelTab[0]).toggleClass("open closed");
+            
+            self.verifyLayout(panel[0]);
         }
         
-        this.onResize = function() {
-        }
-        
-        this.openSubPanel = function(element) {
-            if (element && !jQuery(element).hasClass("open")) {
-                jQuery(element).toggleClass("open closed");
+        this.openSubPanel = function(subpanel) {
+            if (subpanel && !jQuery(subpanel).hasClass("open")) {
+                jQuery(subpanel).toggleClass("open closed");
                 
-                var container = jQuery(element).nextAll("td.pantab-container");
+                var container = jQuery(subpanel).nextAll("td.pantab-container");
                 var panelTab = jQuery(container[0]).children("div.pantab");
                 jQuery(panelTab[0]).toggleClass("open closed");
+                
+                self.verifyLayout(subpanel);
+            }
+        }
+        
+        this.close = function() {
+        }
+        
+        this.verifyLayout = function(panel) {
+            if (jQuery(panel).hasClass("open")) {
+                // Is there enough room? Does anything need to be closed?
+                var screenWidth = jQuery(window).width();
+                var tableWidth = jQuery("#"+self.config.container).width();
+                
+                if (tableWidth > screenWidth) {
+                    var parent = panel;
+                    
+                    var parents = jQuery(panel).parents("td.panel-container");
+                    if (parents && parents.length)
+                        parent = parents[0];
+
+                    var a = jQuery("#"+self.config.container).find("td.panel-container.open");
+                    for (var i = a.length - 1; i >= 0 && tableWidth > screenWidth; i--) {
+                        var p = a[i];
+                        if (panel != p && parent != p) {
+                            // close it
+                            jQuery(p).toggleClass("open closed");
+                            
+                            var container = jQuery(p).nextAll("td.pantab-container");
+                            var panelTab = jQuery(container[0]).children("div.pantab");
+                            jQuery(panelTab[0]).toggleClass("open closed");
+                            
+                            tableWidth = jQuery("#"+self.config.container).width();
+                        }
+                    }
+                    
+                    // @todo -- once discussion is in place and we have a 3 column solution 
+                    // Code should break on panel != parent, then start closing from the right, rather than from the left.
+                }
             }
         }
         

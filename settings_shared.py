5267c8af5f351bab36222cbe81b7abfc1418773c
sdreher <sdreher@gmail.com>
2012-02-09 12:21:39 -0500

    Updating version number.

--- settings_shared.py
+++ settings_shared.py
@@ -13,7 +13,7 @@ ADMINS = (
     ('admin', 'mediathread@example.com'),
 )
 
-REVISION = 5 # introducing a script version to provide cache busting
+REVISION = 6 # introducing a script version to provide cache busting
 
 MANAGERS = ADMINS
 

50dec4addcc6ab6b656135d5d794ef64381b1ea1
sdreher <sdreher@gmail.com>
2012-04-21 20:31:53 -0400

    JSHint'ing

--- media/js/panel.js
+++ media/js/panel.js
@@ -1,47 +1,51 @@
-(function() {
-    
+(function () {
+
     window.PanelFactory = new (function PanelFactoryAbstract() {
-        this.create = function(type, json) {
+        this.create = function (type, json) {
             // Instantiate the panel's handler
             var handler = null;
-            if (type == "project")
+            if (type === "project") {
                 handler = new ProjectPanelHandler(json);
+            }
             
             return handler;
-        }
+        };
     })();
     
     window.PanelManager = new (function PanelManagerAbstract() {
         var self = this;
       
-        this.init = function(options, panels) {
+        this.init = function (options, panels) {
             self.options = options;
             self.panelViews = [];
             
             // Create an assetview.
-            // @todo - We have potentially more than 1 assetview on the project page. The singleton nature in the 
+            // @todo - We have potentially more than 1 assetview on the project page. The singleton nature in the
             // core architecture means the two views are really sharing the underlying code.
             // Consider how to resolve this contention. (It's a big change in the core.)
             
             // This may be DANGEROUS in any sense. The old assetview should be destroyed first?
-            if (!djangosherd.assetview)
-                djangosherd.assetview = new Sherd.GenericAssetView({ clipform:false, clipstrip: true});
+            if (!djangosherd.assetview) {
+                djangosherd.assetview = new Sherd.GenericAssetView({ clipform: false, clipstrip: true});
+            }
             
-            if (!djangosherd.storage)
+            if (!djangosherd.storage) {
                 djangosherd.storage = new DjangoSherd_Storage();
+            }
             
             jQuery.ajax({
                 url: options.url,
-                dataType:'json',
+                dataType: 'json',
                 cache: false, // Chrome && Internet Explorer has aggressive caching policies.
-                success: function(json) {
+                success: function (json) {
                     self.panels = json.panels;
                     self.loadTemplates(0);
-            }});
-        }
+                }
+            });
+        };
         
-        this.loadTemplates = function(idx) {
-            if (idx == self.panels.length) {
+        this.loadTemplates = function (idx) {
+            if (idx === self.panels.length) {
                 // done. load content.
                 self.loadContent();
             } else if (MediaThread.templates[self.panels[idx].template]) {
@@ -50,17 +54,18 @@
             } else {
                 // pull it off the wire
                 jQuery.ajax({
-                    url:'/site_media/templates/' + self.panels[idx].template + '.mustache?nocache=v3',
-                    dataType:'text',
+                    url: '/site_media/templates/' + self.panels[idx].template + '.mustache?nocache=v3',
+                    dataType: 'text',
                     cache: false, // Chrome && Internet Explorer have aggressive caching policies.
-                    success:function(text) {
-                        MediaThread.templates[self.panels[idx].template] = Mustache.template(self.panels[idx].template,text);
+                    success: function (text) {
+                        MediaThread.templates[self.panels[idx].template] = Mustache.template(self.panels[idx].template, text);
                         self.loadTemplates(++idx);
-                }});
+                    }
+                });
             }
-        }
+        };
         
-        this.loadContent = function() {
+        this.loadContent = function () {
             for (var i = 0; i < self.panels.length; i++) {
                 var panel = self.panels[i];
                  
@@ -69,19 +74,19 @@
                 // and makes the sliding UI work nicely
                 jQuery("#" + self.options.container + " tr:first td:last")
                     .before(Mustache.tmpl(panel.template, panel))
-                    .show("slow", function() {
+                    .show("slow", function () {
                         var handler = PanelFactory.create(self.panels[i].context.type, self.panels[i]);
                         self.panelViews.push(handler);
                     });
             }
             
             // enable open/close controls
-            jQuery(".pantab-container").click(function(event) {
+            jQuery(".pantab-container").click(function (event) {
                 self.slidePanel(this, event);
             });
-        }
+        };
         
-        this.slidePanel = function(pantab_container, event) {
+        this.slidePanel = function (pantab_container, event) {
             // Open/close this panhandle's panel
             var panel = jQuery(pantab_container).prevAll("td.panel-container");
             jQuery(panel[0]).toggleClass("open closed");
@@ -90,9 +95,9 @@
             jQuery(panelTab[0]).toggleClass("open closed");
             
             self.verifyLayout(panel[0]);
-        }
+        };
         
-        this.openSubPanel = function(subpanel) {
+        this.openSubPanel = function (subpanel) {
             if (subpanel && !jQuery(subpanel).hasClass("open")) {
                 jQuery(subpanel).toggleClass("open closed");
                 
@@ -102,28 +107,29 @@
                 
                 self.verifyLayout(subpanel);
             }
-        }
+        };
         
-        this.close = function() {
-        }
+        this.close = function () {
+        };
         
-        this.verifyLayout = function(panel) {
+        this.verifyLayout = function (panel) {
             if (jQuery(panel).hasClass("open")) {
                 // Is there enough room? Does anything need to be closed?
                 var screenWidth = jQuery(window).width();
-                var tableWidth = jQuery("#"+self.options.container).width();
+                var tableWidth = jQuery("#" + self.options.container).width();
                 
                 if (tableWidth > screenWidth) {
                     var parent = panel;
                     
                     var parents = jQuery(panel).parents("td.panel-container");
-                    if (parents && parents.length)
+                    if (parents && parents.length) {
                         parent = parents[0];
+                    }
 
-                    var a = jQuery("#"+self.options.container).find("td.panel-container.open");
+                    var a = jQuery("#" + self.options.container).find("td.panel-container.open");
                     for (var i = a.length - 1; i >= 0 && tableWidth > screenWidth; i--) {
                         var p = a[i];
-                        if (panel != p && parent != p) {
+                        if (panel !== p && parent !== p) {
                             // close it
                             jQuery(p).toggleClass("open closed");
                             
@@ -131,15 +137,15 @@
                             var panelTab = jQuery(container[0]).children("div.pantab");
                             jQuery(panelTab[0]).toggleClass("open closed");
                             
-                            tableWidth = jQuery("#"+self.options.container).width();
+                            tableWidth = jQuery("#" + self.options.container).width();
                         }
                     }
                     
-                    // @todo -- once discussion is in place and we have a 3 column solution 
+                    // @todo -- once discussion is in place and we have a 3 column solution
                     // Code should break on panel != parent, then start closing from the right, rather than from the left.
                 }
             }
-        }
+        };
         
     })();
 })();

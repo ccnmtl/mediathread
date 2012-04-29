c421538d0a481f8770ca812c1b9114eb367efa55
sdreher <sdreher@gmail.com>
2012-04-29 12:36:20 -0400

    * speed up the discussion fetching. by a lot.

--- discussions/utils.py
+++ discussions/utils.py
@@ -4,6 +4,7 @@ from django.contrib import comments
 from django.contrib.comments.models import COMMENT_MAX_LENGTH
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.comments.managers import CommentManager
 
 from django.core import urlresolvers
 from django.conf import settings
@@ -13,15 +14,25 @@ Collaboration = get_model('structuredcollaboration', 'collaboration')
 ContentType = get_model('contenttypes','contenttype')
 
 
-def get_discussions( arbitrary_object):
+def get_discussions(arbitrary_object):
     coll = ContentType.objects.get_for_model(Collaboration)
     discussions = []
-    for d in ThreadedComment.objects.filter(parent=None, content_type = coll):
+    comments = ThreadedComment.objects.filter(parent=None, content_type=coll)
+    
+    for d in comments:
         if d.content_object and d.content_object._parent_id and \
                 arbitrary_object == d.content_object._parent.content_object:
             discussions.append(d)
     return discussions  
 
+def get_course_discussions(course):
+    parent = Collaboration.objects.get(object_pk=course.id)
+    colls = Collaboration.objects.filter(_parent=parent)
+    discussions = []
+    for c in colls:
+        discussions.append(c.content_object)
+    return discussions
+        
 def threaded_comment_json(comment):
     data = { 
         'type': 'discussion',

20e4f559c366c0aea16598b7cf4ac57480aa9e06
sdreher <sdreher@gmail.com>
2012-05-06 13:24:38 -0400

    and a little refactoring to try to get the view naming consistent.

--- discussions/urls.py
+++ discussions/urls.py
@@ -1,18 +1,14 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
+
 urlpatterns = patterns(
     'discussions.views', 
 
-    #create a new discussion of an arbitrary model instance:
-    url(r'^new/$', 'new', name='new-discussion'),
+    url(r'^new/$', 'discussion_create', name='discussion-create'),
+    
+    url(r'^delete/(?P<discussion_id>\d+)$', 'discussion_delete', name='discussion-delete'),
 
-    url(r'^show/(?P<discussion_id>\d+)$', 'show', 
-        name='show_discussion'),
-
-    url(r'^(?P<discussion_id>\d+)$', 'show', 
-        name='discussion-view'),
-
-    ##SYNC with absolute url in media/js/discussion.js
-    url(r'^comment/(?P<comment_id>\d+)$', 'comment_change', 
-        name='update-discussion-comment'),
-    )
+    url(r'^(?P<discussion_id>\d+)$', 'discussion_view', name='discussion-view'),
+    
+    url(r'^comment/(?P<comment_id>\d+)$', 'comment_save', name='comment-save'),
+)

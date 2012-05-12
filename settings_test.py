c87de0e083ba55c98e5da49a8d8a4c1a1d370ab5
sdreher <sdreher@gmail.com>
2012-05-12 11:25:25 -0400

    * basic homepage tests.

--- settings_test.py
+++ settings_test.py
@@ -6,3 +6,10 @@ DATABASE_NAME = 'lettuce.db'
 # Required in path: sqlite3 and Firefox
 # Run ./bootstrapy.py --full to ensure necessary lxml dependencies are built
 # ./manage.py harvest --settings=settings_test
+
+# Test data is automatically bootstrapped before the entire test run. 
+# Some basic data is available
+# Sample Course
+# test_instructor / test
+# test_student_one / test
+# test_student_two / test

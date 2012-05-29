35163de916e9cfb3d1527d9d176d6cdb4dd33d6b
sdreher <sdreher@gmail.com>
2012-05-29 14:12:47 -0400

    Updating stage settings.

--- settings_stage.py
+++ settings_stage.py
@@ -10,3 +10,8 @@ MEDIA_ROOT = '/usr/local/share/sandboxes/common/mediathread/uploads/'
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+try:
+    from local_settings import *
+except ImportError:
+    pass

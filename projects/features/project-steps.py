4ad00b770a5785b931c8a2229c860fe89879c9b0
sdreher <sdreher@gmail.com>
2012-08-27 14:08:55 -0400

    * text tweaks * Instructor Dashboard Create Composition / Create Discussion tests.

--- projects/features/project-steps.py
+++ projects/features/project-steps.py
@@ -10,20 +10,7 @@ from urlparse import urlparse
 def there_are_no_projects(step):
     assert len(Project.objects.all()) == 0
 
-@step(u'there is an? ([^"]*) ([^"]*) panel')
-def there_is_a_state_name_panel(step, state, name):
-    """
-    Keyword arguments:
-    state -- open, closed
-    name -- composition, assignment, discussion, collection 
 
-    """
-    try:
-        panel = world.firefox.find_element_by_css_selector("td.panel-container.%s.%s" % (state.lower(), name.lower()))
-    except:
-        time.sleep(1)
-        panel = world.firefox.find_element_by_css_selector("td.panel-container.%s.%s" % (state.lower(), name.lower()))
-    assert panel != None, "Can't find panel named %s" % panel
     
 @step(u'there is not an? ([^"]*) ([^"]*) panel')
 def there_is_not_a_state_name_panel(step, state, name):
@@ -67,19 +54,6 @@ def the_panel_does_not_have_a_name_button(step, panel, name):
     except:
         pass # expected
     
-@step(u'I call the ([^"]*) "([^"]*)"')
-def i_call_the_panel_title(step, panel, title):
-    try:
-        panel = world.firefox.find_element_by_css_selector("td.panel-container.open.%s" % panel.lower())
-    except:
-        time.sleep(1)
-        panel = world.firefox.find_element_by_css_selector("td.panel-container.open.%s" % panel.lower())
-    assert panel != None, "Can't find panel named %s" % panel
-    
-    input = panel.find_element_by_name("title")
-    input.clear()
-    input.send_keys(title)
-    
 @step(u'The ([^"]*) title is "([^"]*)"')
 def the_panel_title_is_value(step, panel, value):
     try:
@@ -92,19 +66,7 @@ def the_panel_title_is_value(step, panel, value):
     h1 = panel.find_element_by_css_selector("h1.project-title")
     assert h1.text.strip() == value, "Expected %s title %s. Found %s" % (panel, value, h1.text.strip())
         
-@step(u'I write some text for the ([^"]*)')
-def i_write_some_text_for_the_panel(step, panel):
-    panel = world.firefox.find_element_by_css_selector("td.panel-container.open.%s" % panel.lower())
-    assert panel != None, "Can't find panel named %s" % panel
-    
-    frame = panel.find_element_by_tag_name("iframe")
-    world.firefox.switch_to_frame(frame)
-    input = world.firefox.find_element_by_class_name("mceContentBody")
-    input.send_keys("""The Columbia Center for New Teaching and Learning was (CCNMTL)
-                    was founded at Columbia University in 1999 to enhance teaching and
-                    learning through the purposeful use of new media and technology""")
-    
-    world.firefox.switch_to_default_content()
+
     
 @step(u'I see a ([^"]*) dialog')
 def i_see_a_name_dialog(step, name):
@@ -172,53 +134,6 @@ def i_select_username_s_response(step, username):
             return
     assert False, "Unable to find a response for %s" % username
 
-  
-     
-    
-@step(u'i save the changes')
-def i_save_the_changes(step):
-    elts = world.firefox.find_elements_by_tag_name("button")
-    for e in elts:
-        if e.get_attribute("type") == "button" and e.text == "Save":
-            e.click()
-            time.sleep(1)
-            return
-        
-    assert False, "Unable to locate the dialog's save button"
-    
-@step(u'there is an? ([^"]*) "([^"]*)" project by ([^"]*)')
-def there_is_a_status_title_project_by_author(step, status, title, author):
-    elts = world.firefox.find_elements_by_css_selector("li.projectlist")
-    if len(elts) < 1:
-        time.sleep(1)
-        elts = world.firefox.find_elements_by_css_selector("li.projectlist")
-    assert len(elts) > 0, "Expected to find at least 1 project. Instead there are none"
-    
-    assignment = False
-    for e in elts:
-        try:
-            title_elt = e.find_element_by_css_selector("a.asset_title.type-project")
-        except:
-            title_elt = e.find_element_by_css_selector("a.asset_title.type-assignment")
-            assignment = True
-            
-        if title_elt.text.strip() == title:
-            if not assignment:
-                # author
-                author_elt = e.find_element_by_css_selector("span.metadata-value-author")
-                msg = "%s author is [%s]. Expected [%s]." % (title, author_elt.text.strip(), author)
-                assert author_elt.text.strip() == author, msg
-                
-            # status            
-            status_elt = e.find_element_by_css_selector("span.metadata-value-status")
-            msg = "%s status starts with [%s]. Expected [%s]" % (title, status_elt.text.strip().lower(), status)
-            assert status_elt.text.strip().lower().startswith(status), msg 
-            
-            return
-            
-    assert False, "Unable to find project named %s" % title
-    
-    
 @step(u'there is a comment that begins "([^"]*)"')
 def there_is_a_comment_that_begins_text(step, text):
     elts = world.firefox.find_elements_by_css_selector("div.threaded_comment_text")

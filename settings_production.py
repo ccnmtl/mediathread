1dd84034ff4d35fc50265bc5380bbd462f34086f
sdreher <sdreher@gmail.com>
2012-11-30 12:38:19 -0500

    Sentry configuration goes only in production configuration.

--- settings_production.py
+++ settings_production.py
@@ -20,6 +20,25 @@ TEMPLATE_DEBUG = DEBUG
 
 WIND_SERVICE = "cnmtl_full_p"
 
+# IMPORTANT: make sure your local_settings.py file
+# has a SENTRY_KEY defined
+# as documented in the wiki
+SENTRY_SITE = 'mediathread'
+SENTRY_SERVERS = ['http://sentry.ccnmtl.columbia.edu/sentry/store/']
+
+import logging
+from raven.contrib.django.handlers import SentryHandler
+
+logger = logging.getLogger()
+# ensure we havent already registered the handler
+if SentryHandler not in map(type, logger.handlers):
+    logger.addHandler(SentryHandler())
+
+    # Add StreamHandler to sentry's default so you can catch missed exceptions
+    logger = logging.getLogger('sentry.errors')
+    logger.propagate = False
+    logger.addHandler(logging.StreamHandler())
+
 try:
     from local_settings import *
 except ImportError:

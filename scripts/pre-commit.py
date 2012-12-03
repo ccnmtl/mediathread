20cb934bad949de6c41881074a4b692733e81491
sdreher <sdreher@gmail.com>
2012-12-03 14:35:45 -0500

    pre-commit tweaks

--- scripts/pre-commit.py
+++ scripts/pre-commit.py
@@ -22,6 +22,7 @@ CHECKS = [
         'match_files': ['.*\.py$'],
         'ignore_files': ['.*migrations.*', '.*management/commands.*',
                          '.*manage.py', '.*/scripts/.*', '.*/ve/.*',
+                         '.*scripts/pre-commit\.py$',
                          '.*virtualenv\.py$'],
         'print_filename': True,
     },
@@ -29,9 +30,9 @@ CHECKS = [
         'output': 'Checking for console.log()...',
         'command': 'grep -n console.log %s',
         'match_files': ['.*\.js$'],
-        'ignore_files' : ['.*backbone-min\.js$', '.*jquery-.*\.js$',
-                          '.*underscore.*\.js$', '.*/media/CACHE/.*',
-                          '.*/ve/.*'],
+        'ignore_files': ['.*backbone-min\.js$', '.*jquery-.*\.js$',
+                         '.*underscore.*\.js$', '.*/media/CACHE/.*',
+                         '.*/ve/.*'],
         'print_filename': True,
     },
     {
@@ -63,22 +64,27 @@ CHECKS = [
 
 def matches_file(file_name, match_files):
     return any(re.compile(match_file).match(file_name)
-                for match_file in match_files)
+               for match_file in match_files)
 
 
 def check_files(files, check):
     result = 0
     for file_name in files:
-        if not 'match_files' in check or matches_file(file_name, check['match_files']):
-            if not 'ignore_files' in check or not matches_file(file_name, check['ignore_files']):
-                process = subprocess.Popen(check['command'] % file_name, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        if (not 'match_files' in check or
+                matches_file(file_name, check['match_files'])):
+            if (not 'ignore_files' in check or
+                    not matches_file(file_name, check['ignore_files'])):
+                process = subprocess.Popen(check['command'] % file_name,
+                                           stdout=subprocess.PIPE,
+                                           stderr=subprocess.PIPE, shell=True)
                 out, err = process.communicate()
                 if out or err:
                     if check['print_filename']:
                         prefix = '\t%s:' % file_name
                     else:
                         prefix = '\t'
-                    output_lines = ['%s%s' % (prefix, line) for line in out.splitlines()]
+                    output_lines = ['%s%s' % (prefix, line)
+                                    for line in out.splitlines()]
                     print '\n'.join(output_lines)
                     if err:
                         print err
@@ -89,39 +95,44 @@ def check_files(files, check):
 def main(all_files):
     # Stash any changes to the working tree that are not going to be committed
     subprocess.call(['git', 'stash', '--keep-index'], stdout=subprocess.PIPE)
-    
+
     files = []
     if all_files:
         for root, dirs, file_names in os.walk('.'):
             for file_name in file_names:
                 files.append(os.path.join(root, file_name))
     else:
-        p = subprocess.Popen(['git', 'status', '--porcelain'], stdout=subprocess.PIPE)        
+        p = subprocess.Popen(['git', 'status', '--porcelain'],
+                             stdout=subprocess.PIPE)
         out, err = p.communicate()
         for line in out.splitlines():
             match = modified.match(line)
             if match:
                 files.append(match.group('name'))
-    
+
     result = 0
     print 'Running Django Code Validator...'
     return_code = subprocess.call('./manage.py validate', shell=True)
     result = return_code or result
-    
+
     print 'Running Unit Tests...'
-    return_code = subprocess.call('./manage.py test djangosherd.SherdNoteTest', shell=True)
+    return_code = subprocess.call('./manage.py test djangosherd.SherdNoteTest',
+                                  shell=True)
     result = return_code or result
-    
+
     for check in CHECKS:
         result = check_files(files, check) or result
-        
+
     # Unstash changes to the working tree that we had stashed
-    subprocess.call(['git', 'stash', 'pop', '--quiet', '--index'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
-    # Update the release id
-    print 'Updating Release Id...'
-    return_code = subprocess.call('scripts/update-release-id.sh', shell=True)
-        
+    subprocess.call(['git', 'stash', 'pop', '--quiet', '--index'],
+                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Update the release id if things looks good
+    if result == 0:
+        print 'Updating Release Id...'
+        return_code = subprocess.call('scripts/update-release-id.sh',
+                                      shell=True)
+
     sys.exit(result)
 
 

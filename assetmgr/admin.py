77ea9eafc971620ba9e2aae8af3f6c00fb858b67
sdreher <sdreher@gmail.com>
2012-12-07 09:38:02 -0500

    flake8 assetmgr

--- assetmgr/admin.py
+++ assetmgr/admin.py
@@ -1,15 +1,15 @@
 from mediathread.assetmgr.models import Asset, Source, SupportedSource
 from django.contrib import admin
 
+
 class AssetAdmin(admin.ModelAdmin):
     class Meta:
         model = Asset
-        
-    change_form_template="assetmgr/admin_change_form.html"
-    search_fields = ("title","course__title","source__url",)
-    list_display = ("title","id","course","added","modified","active")
 
-admin.site.register(Asset,AssetAdmin)
+    change_form_template = "assetmgr/admin_change_form.html"
+    search_fields = ("title", "course__title", "source__url",)
+    list_display = ("title", "id", "course", "added", "modified", "active")
+
+admin.site.register(Asset, AssetAdmin)
 admin.site.register(Source)
 admin.site.register(SupportedSource)
-

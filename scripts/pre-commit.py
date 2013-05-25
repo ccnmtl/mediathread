c10f1dab6d11c6ae9509f46be05544496f5109e3
Susan Dreher <sdreher@columbia.edu>
2013-05-25 10:21:35 -0400

    reactivating unit tests

--- scripts/pre-commit.py
+++ scripts/pre-commit.py
@@ -120,10 +120,10 @@ def main(all_files):
 
     if result == 0:
         print 'Running Unit Tests...'
-        #return_code = subprocess.call(
-        #    './manage.py test djangosherd assetmgr projects mediathread_main',
-        #    shell=True)
-        #result = return_code or result
+        return_code = subprocess.call(
+            './manage.py test djangosherd assetmgr projects mediathread_main',
+            shell=True)
+        result = return_code or result
 
     # Unstash changes to the working tree that we had stashed
     subprocess.call(['git', 'stash', 'pop', '--quiet', '--index'],

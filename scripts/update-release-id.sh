a9fc907ffb2662b328ba0a2f003203112b053b3a
Susan Dreher <sdreher@columbia.edu>
2013-06-01 14:16:17 -0400

    fixing release id generation

--- scripts/update-release-id.sh
+++ scripts/update-release-id.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# link me into your git hooks like so:
-# ln -s `pwd`/scripts/pre-commit `pwd`/.git/hooks/pre-commit
 branch="`git rev-parse --symbolic-full-name HEAD`"
 echo "LAST_GIT_HEAD = \"`cat .git/$branch`\"" > mediathread/release_id.py
-git add release_id.py
+git add mediathread/release_id.py

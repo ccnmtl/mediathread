60fe1177261d74d51a7b07fde1c330cf32006efb
LittleQ <littleq0903@gmail.com>
2013-07-27 04:32:21 +0800

    form_valid for registration form (not completed)

--- mediathread/accounts/views.py
+++ mediathread/accounts/views.py
@@ -7,6 +7,11 @@ from .forms import InviteStudentsForm
 class RegistrationFormView(FormView):
     form_class = RegistrationForm
     template_name = 'accounts/registration_form.html'
+    success_url = 'accounts/registration_done'
+
+    def form_valid(self, form):
+        # TODO: create an account for it and send out a registration confirmation mail
+        return super(RegistrationFormView, self).form_valid(form)
 
 registration_form = RegistrationFormView.as_view()
 

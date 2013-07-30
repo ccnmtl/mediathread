9e86a3d4e1fdae236f1f4a4fb2a37516062fa647
Filip Jukic <filip@jukic.me>
2013-07-30 13:38:12 -0400

    implemented middleware to force invited students to change their password (no custom template yet)

--- mediathread/accounts/urls.py
+++ mediathread/accounts/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import include, patterns, url
+from django.contrib.auth.forms import SetPasswordForm
+from django.contrib.auth.views import password_change
 from .views import *
 
 urlpatterns = patterns('',
+    url(r'^set_password/$',
+        password_change, {
+            'password_change_form': SetPasswordForm,
+            'post_change_redirect': '/'
+        },
+        name="set_password"),
     url(r'^invite_students/$',
         invite_students,
         name="invite-students"),

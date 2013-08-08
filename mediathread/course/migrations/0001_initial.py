6bd779ea60231c2005dd9a506f61d6fd81528c82
Filip Jukic <filip@jukic.me>
2013-08-08 13:45:37 -0400

    added migration dependency

--- mediathread/course/migrations/0001_initial.py
+++ mediathread/course/migrations/0001_initial.py
@@ -6,6 +6,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("user_accounts", "0001_initial"),
+    )
+
     def forwards(self, orm):
         
         # Adding model 'CourseInformation'

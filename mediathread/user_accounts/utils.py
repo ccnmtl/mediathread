bffe191ad70424f7002131ce1f9fb59edcd7c77b
Filip Jukic <filip@jukic.me>
2013-08-09 16:12:02 -0400

    added display_user method that shows users full name if possible, username otherwise

--- mediathread/user_accounts/utils.py
+++ mediathread/user_accounts/utils.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 import mailsnake
 
+
 def add_email_to_mailchimp_list(email_address, list_id, **kwargs):
     merge_vars_dict = {}
     if kwargs.has_key('fname'):
@@ -14,11 +15,17 @@ def add_email_to_mailchimp_list(email_address, list_id, **kwargs):
 
     ms = mailsnake.MailSnake(settings.MAILCHIMP_API_KEY)
     ms.listSubscribe(
-            id = list_id,
-            email_address = email_address,
-            merge_vars = merge_vars_dict,
-            update_existing = True,
-            double_optin = False)
+        id=list_id,
+        email_address=email_address,
+        merge_vars=merge_vars_dict,
+        update_existing=True,
+        double_optin=False)
 
     return True
 
+
+def display_user(user):
+    if user.first_name or user.last_name:
+        return user.get_full_name()
+    else:
+        return user.username

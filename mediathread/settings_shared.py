1d54c04883f596d09c893c10087e24ca2f72baed
Susan Dreher <sdreher@columbia.edu>
2013-09-04 16:09:36 -0400

    Merging in master

--- mediathread/settings_shared.py
+++ mediathread/settings_shared.py
@@ -225,8 +225,18 @@ FORCE_LOWERCASE_TAGS = True
 
 # if you set this to a string, then bookmarklet can import from flickr
 DJANGOSHERD_FLICKR_APIKEY = None
-FLOWPLAYER_SWF_LOCATION = \
-    "http://releases.flowplayer.org/swf/flowplayer-3.2.2.swf"
+
+# Mediathread instantiates a Flowplayer .swf to play many video flavors.
+# Update this variable with your site's Flowplayer installation
+# See README.markdown for more information
+# expected: http://<server>/<directory>/flowplayer-3.2.15.swf
+FLOWPLAYER_SWF_LOCATION = None
+# Specify your own plugin versions here. The player looks in the same
+# http://<server>/<directory>/ specified above.
+FLOWPLAYER_AUDIO_PLUGIN = 'flowplayer.audio-3.2.10.swf'
+FLOWPLAYER_PSEUDOSTREAMING_PLUGIN = 'flowplayer.pseudostreaming-3.2.11.swf'
+FLOWPLAYER_RTMP_PLUGIN = 'flowplayer.rtmp-3.2.11.swf'
+
 
 DEFAULT_COLLABORATION_POLICY = policies.InstructorManaged()
 

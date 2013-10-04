258b07f6d965c8e982a80bc288662724bf76779e
Susan Dreher <sdreher@columbia.edu>
2013-10-04 13:00:49 -0400

    * adding in django toolbar by default. * start on autosave.

--- mediathread/settings_shared.py
+++ mediathread/settings_shared.py
@@ -116,7 +116,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.transaction.TransactionMiddleware',
     'courseaffils.middleware.CourseManagerMiddleware',
     'mediathread.main.middleware.AuthRequirementMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware'
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'mediathread.urls'
@@ -161,8 +162,23 @@ INSTALLED_APPS = [
     'django_jenkins',
     'mediathread.taxonomy',
     'django.contrib.staticfiles',
-    'smoketest'
+    'smoketest',
+    'debug_toolbar',
 ]
+
+INTERNAL_IPS = ('127.0.0.1', )
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
 
 COMPRESS_URL = "/site_media/"
 COMPRESS_ROOT = "media/"

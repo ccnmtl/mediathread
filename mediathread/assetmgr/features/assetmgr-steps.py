058966cfbc8e7d3e8561a1b77874f5735fdfddf1
Susan Dreher <sdreher@columbia.edu>
2013-10-07 12:22:38 -0400

    One more "change asset title" selenium test

--- mediathread/assetmgr/features/assetmgr-steps.py
+++ mediathread/assetmgr/features/assetmgr-steps.py
@@ -1,5 +1,6 @@
 from lettuce import world, step
 import time
+from selenium.common.exceptions import InvalidElementStateException
 
 
 @step(u'The item header is "([^"]*)"')
@@ -7,8 +8,10 @@ def the_item_header_is_name(step, name):
     if world.using_selenium:
         selector = "span.asset-view-title"
         elt = world.browser.find_element_by_css_selector(selector)
-        assert elt.text.strip() == name, \
-            "The title was %s. Expected %s" % (elt.text, name)
+        if elt.text.strip() != name:
+            time.sleep(2)
+            assert elt.text.strip() == name, \
+                "The title was %s. Expected %s" % (elt.text, name)
 
 
 @step(u'I edit the item')
@@ -27,7 +30,7 @@ def i_edit_the_item(step):
 
 
 @step(u'I set the "([^"]*)" "([^"]*)" field to "([^"]*)"')
-def i_set_the_label_type__to_value(step, label, type, value):
+def i_set_the_label_type_to_value(step, label, type, value):
     if world.using_selenium:
         parent = world.browser.find_element_by_id("asset-view-details")
         if type == "text":
@@ -37,6 +40,26 @@ def i_set_the_label_type__to_value(step, label, type, value):
         elts = parent.find_elements_by_css_selector(selector)
         for elt in elts:
             if elt.get_attribute('data-label') == label:
-                elt.clear()
-                elt.send_keys(value)
-                return
+                try:
+                    elt.clear()
+                    elt.send_keys(value)
+                    return
+                except InvalidElementStateException:
+                    time.sleep(1)
+                    elt.clear()
+                    elt.send_keys(value)
+
+
+@step(u'there is not a "([^"]*)" "([^"]*)" field')
+def there_is_not_a_label_type_field(step, label, type):
+    if world.using_selenium:
+        parent = world.browser.find_element_by_id("asset-view-details")
+        if type == "text":
+            selector = "input[type=text]"
+        elif type == "textarea":
+            selector = "textarea"
+
+        elts = parent.find_elements_by_css_selector(selector)
+        for elt in elts:
+            if elt.get_attribute('data-label') == label:
+                assert False, "Found a %s %s field" % (label, type)
--- mediathread/assetmgr/features/single_asset_view.feature
+++ mediathread/assetmgr/features/single_asset_view.feature
@@ -43,12 +43,43 @@ Feature: Single Asset View
         And I set the "Notes" "textarea" field to "Here are my notes"
         And I click the Save button
         
-        Then I wait 2 seconds
         Then the item header is "Updated MAAP Award Reception"
         And I see "Here are my notes"
         And I see "abc"
         And I do not see "instructor one item note"
         And I do not see "flickr, instructor_one"
-        
                         
-        Finished using Selenium         
+        Finished using Selenium
+        
+    Scenario: single_asset_view.feature 3. Edit global annotation as student
+        Using selenium
+        Given I am test_student_one in Sample Course
+        
+        When I access the url "/asset/2/"
+        Then there is a minimized Collection panel        
+        And there is an open Asset panel
+        And the asset workspace is loaded
+        
+        # Verify the asset is really there
+        The item header is "MAAP Award Reception"
+        And I see "student one item note"
+        And I see "student_one_item"
+        And I do not see "Here are my notes"
+        And I do not see "abc"
+        
+        When I edit the item
+        Then there is a Cancel button
+        And there is a Save button
+        And there is not a "Title" "text" field
+        
+        And I set the "Tags" "text" field to "abc"
+        And I set the "Notes" "textarea" field to "Here are my notes"
+        And I click the Save button
+        
+        Then the item header is "MAAP Award Reception"
+        And I see "Here are my notes"
+        And I see "abc"
+        And I do not see "student one item note"
+        And I do not see "student_one_item"
+                        
+        Finished using Selenium

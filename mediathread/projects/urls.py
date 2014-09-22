8fe3422a861b310ca187df7b7561a1362f5d3d19
Anders Pearson <anders@columbia.edu>
2014-09-22 20:57:50 +0200

    Django -> 1.6.5
    tests break due to outdated fixtures, which I do not know how to upgrade

--- mediathread/projects/urls.py
+++ mediathread/projects/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from mediathread.projects.views import ProjectCreateView
 
 
--- scripts/dump-test-db.sh
+++ scripts/dump-test-db.sh
@@ -1,2 +1,2 @@
-./manage.py dumpdata --settings=settings_test --indent=4 > main/fixtures/sample_course.json
-./manage.py dumpdata --settings=settings_test --exclude contenttypes --exclude tagging --indent=4 > main/fixtures/unittest_sample_course.json
+./manage.py dumpdata --settings=mediathread.settings_test --exclude --indent=4 > mediathread/main/fixtures/sample_course.json
+./manage.py dumpdata --settings=mediathread.settings_test --exclude contenttypes --exclude tagging --indent=4 > mediathread/main/fixtures/unittest_sample_course.json

ade1ea2297509b53e527e2793f9c014f839c291c
Anders Pearson <anders@columbia.edu>
2014-09-23 14:24:30 +0200

    captcha.js passes jscs

--- media/js/app/captcha.js
+++ media/js/app/captcha.js
@@ -1,8 +1,8 @@
-jQuery(document).ready(function () {
-    jQuery('.js-captcha-refresh').click(function(){
+jQuery(document).ready(function() {
+    jQuery('.js-captcha-refresh').click(function() {
         var form = jQuery(this).parents('form');
 
-        jQuery.getJSON("/captcha/refresh/", {}, function(json) {
+        jQuery.getJSON('/captcha/refresh/', {}, function(json) {
             jQuery(form).find('input[name="captcha_0"]').val(json.key);
             jQuery(form).find('img.captcha').attr('src', json.image_url);
         });

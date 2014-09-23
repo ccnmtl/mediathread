104434e74d6081fdcce02c81834d29294dbc66a5
Anders Pearson <anders@columbia.edu>
2014-09-23 14:23:38 +0200

    ajaxDelete.js passes jscs

--- media/js/app/ajaxDelete.js
+++ media/js/app/ajaxDelete.js
@@ -7,61 +7,63 @@ function ajaxDelete(link, container, opts) {
     } else if (opts && opts.href) {
         postUrl = opts.href;
     } else {
-        showMessage("An error occurred. Unable to delete this item.");
-    }    
-    
+        showMessage('An error occurred. Unable to delete this item.');
+    }
+
     var dom = document.getElementById(container);
     jQuery(dom).addClass('about-to-delete');
-    
-    var msg = "Are you sure you want to delete this?";
+
+    var msg = 'Are you sure you want to delete this?';
     if (opts) {
         if (opts.msg) {
             msg = opts.msg;
         } else if (opts.item) {
-            msg = "Are you sure you want to remove this item from your collection?";            
+            msg = 'Are you sure you want to remove this item ' +
+                  'from your collection?';
         } else if (opts.object_type) {
-            msg = "Are you sure you want to delete this " + opts.object_type + "?";
+            msg = 'Are you sure you want to delete this ' +
+                  opts.object_type + '?';
         }
     }
-    
-    jQuery("#dialog-confirm").html(msg);
-    
-    jQuery("#dialog-confirm").dialog({
+
+    jQuery('#dialog-confirm').html(msg);
+
+    jQuery('#dialog-confirm').dialog({
         resizable: false,
         modal: true,
-        title: "Confirm action",
+        title: 'Confirm action',
         close: function(event, ui) {
             jQuery(dom).removeClass('about-to-delete');
         },
         buttons: {
-            "Cancel": function() {
-                jQuery(this).dialog("close");                
+            'Cancel': function() {
+                jQuery(this).dialog('close');
             },
-            "OK": function() {
-                jQuery(this).dialog("close");
+            'OK': function() {
+                jQuery(this).dialog('close');
                 jQuery.ajax({
                     type: 'POST',
                     url: postUrl,
-                    success: function (responseText, textStatus, xhr) {
+                    success: function(responseText, textStatus, xhr) {
                         if (xhr.status === 200) {
-                            jQuery(dom).fadeOut(function () {
+                            jQuery(dom).fadeOut(function() {
                                 jQuery(dom).remove();
                             });
                         } else {
-                            showMessage("Error: " + textStatus);
+                            showMessage('Error: ' + textStatus);
                         }
                         if (opts && opts.success) {
                             opts.success();
                         }
                     },
-                    error: function (xhr) {
+                    error: function(xhr) {
                         window.sky = xhr;
-                        showMessage("Error!");
+                        showMessage('Error!');
                     }
-                });                    
+                });
             }
         }
     });
-    
+
     return false;
 }

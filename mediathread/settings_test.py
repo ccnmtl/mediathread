016aa0260067e325d35707f1487edcea5c580658
Susan Dreher <sdreher@columbia.edu>
2015-04-08 19:45:05 -0400

    Relative path to lettuce.db

--- mediathread/settings_test.py
+++ mediathread/settings_test.py
@@ -1,13 +1,10 @@
 # flake8: noqa
 from settings_shared import *
-import os
-
-PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_PATH, '../lettuce.db'),
+        'NAME': './lettuce.db',
         'OPTIONS': {
             'timeout': 30,
         },
@@ -15,9 +12,9 @@ DATABASES = {
     }
 }
 
-#BROWSER = 'Firefox'
+# BROWSER = 'Firefox'
 BROWSER = 'Headless'
-#BROWSER = 'Chrome'
+# BROWSER = 'Chrome'
 
 LETTUCE_APPS = (
     'mediathread.main',

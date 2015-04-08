909615cbc2f89ef3f8d97c176619ebd3a2fa0556
Susan Dreher <sdreher@columbia.edu>
2015-04-08 19:23:20 -0400

    strip down settings_test.py

--- mediathread/settings_test.py
+++ mediathread/settings_test.py
@@ -2,8 +2,6 @@
 from settings_shared import *
 import os
 
-DEBUG = True
-
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 DATABASES = {
@@ -17,7 +15,6 @@ DATABASES = {
     }
 }
 
-LETTUCE_SERVER_PORT = 8002
 #BROWSER = 'Firefox'
 BROWSER = 'Headless'
 #BROWSER = 'Chrome'
@@ -31,30 +28,3 @@ LETTUCE_APPS = (
 
 LETTUCE_DJANGO_APP = ['lettuce.django']
 INSTALLED_APPS = INSTALLED_APPS + LETTUCE_DJANGO_APP
-
-try:
-    from local_settings import COMPRESS_ROOT
-except ImportError:
-    COMPRESS_ROOT = "/Users/sdreher/workspace/mediathread/media/"
-
-
-# Full run
-# time(./manage.py harvest --settings=mediathread.settings_test \
-# --debug-mode --verbosity=2 --traceback)
-
-# Run a particular file + scenario
-# ./manage.py harvest \
-# mediathread/main/features/manage_selection_visibility.feature \
-# -d --settings=mediathread.settings_test -s 1
-
-class ExceptionLoggingMiddleware(object):
-    def process_exception(self, request, exception):
-        import traceback
-        print traceback.format_exc()
-
-MIDDLEWARE_CLASSES.append(
-    'mediathread.settings_test.ExceptionLoggingMiddleware')
-
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False
-}

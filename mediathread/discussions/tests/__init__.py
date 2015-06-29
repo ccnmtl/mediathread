6b752f6658baba73fb4411e7d880770c23f3d6b6
Anders Pearson <anders@columbia.edu>
2015-06-29 15:57:34 +0200

    no longer need to import stuff into tests/__init__.py files for test runner to find them

--- mediathread/discussions/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# pylint: disable-msg=W0401
-# flake8: noqa
-from mediathread.discussions.tests.test_utils import *
-from mediathread.discussions.tests.test_views import *
--- mediathread/taxonomy/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# pylint: disable-msg=W0401
-# flake8: noqa
-from mediathread.taxonomy.tests.test_views import *
-from mediathread.taxonomy.tests.test_api import *

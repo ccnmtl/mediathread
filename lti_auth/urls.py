06774c027a89f5d43a310a7c5cea8c1d012d2977
Susan Dreher <sdreher@columbia.edu>
2015-09-26 09:37:27 -0400

    LTI Configuration View

--- lti_auth/urls.py
+++ lti_auth/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
 
-from lti_auth.views import LTILoginView
+from lti_auth.views import LTILoginView, LTIConfigView
+
 
 urlpatterns = patterns(
     '',
+    url(r'^config/$', LTIConfigView.as_view(), {}, 'lti-config'),
     url(r'^$', LTILoginView.as_view(), {}, 'lti-login'),
 )

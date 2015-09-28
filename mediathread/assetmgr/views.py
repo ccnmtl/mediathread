2edd3741f0807aec885706a1eb1bbae4a30161da
Susan Dreher <sdreher@columbia.edu>
2015-09-28 14:39:51 -0400

    a bit more refactoring

--- mediathread/assetmgr/views.py
+++ mediathread/assetmgr/views.py
@@ -482,47 +482,32 @@ class AssetWorkspaceView(LoggedInMixin, RestrictedMaterialsMixin,
                          JSONResponseMixin, View):
 
     def get(self, request, asset_id=None, annot_id=None):
-        if asset_id:
-            try:
-                asset = Asset.objects.get(pk=asset_id, course=request.course)
-                asset.primary
-            except Asset.DoesNotExist:
-                return asset_switch_course(request, asset_id)
-            except Source.DoesNotExist:
-                ctx = RequestContext(request)
-                return render_to_response('500.html', {}, context_instance=ctx)
-
         data = {'asset_id': asset_id, 'annotation_id': annot_id}
 
         if not request.is_ajax():
             return render_to_response('assetmgr/asset_workspace.html',
                                       data,
                                       context_instance=RequestContext(request))
-        ctx = {'type': 'asset'}
+        else:
+            vocabulary = VocabularyResource().render_list(
+                request, Vocabulary.objects.get_for_object(request.course))
 
-        vocabulary = VocabularyResource().render_list(
-            request, Vocabulary.objects.get_for_object(request.course))
+            user_resource = UserResource()
+            owners = user_resource.render_list(request, request.course.members)
 
-        user_resource = UserResource()
-        owners = user_resource.render_list(request, request.course.members)
+            data['panels'] = [{
+                'panel_state': 'open',
+                'panel_state_label': "Annotate Media",
+                'context': {'type': 'asset'},
+                'owners': owners,
+                'vocabulary': vocabulary,
+                'template': 'asset_workspace',
+                'current_asset': asset_id,
+                'current_annotation': annot_id,
+                'update_history': True,
+                'show_collection': True}]
 
-        update_history = True
-        show_collection = True
-        template = 'asset_workspace'
-
-        data['panels'] = [{
-            'panel_state': 'open',
-            'panel_state_label': "Annotate Media",
-            'context': ctx,
-            'owners': owners,
-            'vocabulary': vocabulary,
-            'template': template,
-            'current_asset': asset_id,
-            'current_annotation': annot_id,
-            'update_history': update_history,
-            'show_collection': show_collection}]
-
-        return self.render_to_json_response(data)
+            return self.render_to_json_response(data)
 
 
 class AssetDetailView(LoggedInMixin, RestrictedMaterialsMixin,

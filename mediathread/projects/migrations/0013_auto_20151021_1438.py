beb3114649cd0ea3bb2c6d13f6f51880bb8db942
Susan Dreher <sdreher@columbia.edu>
2015-10-21 14:39:20 -0400

    ProjectVersion - remove database tables

---
+++ mediathread/projects/migrations/0013_auto_20151021_1438.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('projects', '0012_auto_20150901_1127'),
+    ]
+
+    operations = [
+        migrations.DeleteModel(
+            name='ProjectVersion',
+        ),
+        migrations.AlterField(
+            model_name='project',
+            name='participants',
+            field=models.ManyToManyField(related_name='projects', verbose_name=b'Authors', to=settings.AUTH_USER_MODEL, blank=True),
+        ),
+    ]

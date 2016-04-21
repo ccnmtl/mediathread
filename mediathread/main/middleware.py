6e97c353faee635b605637ba3479f6aa1a0e3d67
Nik Nyby <nnyby@columbia.edu>
2016-04-21 11:04:48 -0400

    Clean up Instructor Homepage / CourseList naming
    I was confused when naming this feature - I'm not really making a new
    'instructor-specific' page, and in addition the 'homepage' in
    Mediathread refers to the page you're at when you're in a course, not
    the course selection page. So I've renamed some things to try and keep
    things straight:
    
    * The instructor_homepage waffle flag is now called course_activation
    * I'm really just making a new 'CourseList' view, not a new homepage. So
      I've renamed HomepageView to MethCourseListView to differentiate this
      from courseaffils' CourseListView.

--- mediathread/main/middleware.py
+++ mediathread/main/middleware.py
@@ -1,13 +1,13 @@
 import waffle
 from courseaffils.middleware import CourseManagerMiddleware
-from mediathread.main.views import HomepageView
+from mediathread.main.views import MethCourseListView
 
 
 class MethCourseManagerMiddleware(CourseManagerMiddleware):
     def process_request(self, request):
         r = super(MethCourseManagerMiddleware, self).process_request(
             request)
-        if waffle.flag_is_active(request, 'instructor_homepage') and \
+        if waffle.flag_is_active(request, 'course_activation') and \
            r is not None:
-            r = HomepageView.as_view()(request)
+            r = MethCourseListView.as_view()(request)
         return r

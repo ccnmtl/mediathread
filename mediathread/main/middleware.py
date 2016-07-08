12de590aec3753235b8c1b3cd328f974f93d6f81
Susan Dreher <sdreher@columbia.edu>
2016-07-08 12:43:39 -0400

    LTI - explicitly set course
    The LTI link bypasses courseaffils logic and takes the user directly to the course.

--- mediathread/main/middleware.py
+++ mediathread/main/middleware.py
@@ -1,11 +1,24 @@
+from courseaffils.middleware import CourseManagerMiddleware, SESSION_KEY
+from courseaffils.models import Course
 import waffle
-from courseaffils.middleware import CourseManagerMiddleware
+
+from lti_auth.models import LTICourseContext
 from mediathread.main.views import MethCourseListView
 
 
 class MethCourseManagerMiddleware(CourseManagerMiddleware):
     def process_request(self, request):
         override_view = None
+
+        # 'custom_course_context' comes through from lti. set the course.
+        uuid = request.GET.get('custom_course_context', None)
+        if uuid is not None:
+            ctx = LTICourseContext.objects.get(uuid=uuid)
+            course = Course.objects.get(group=ctx.group,
+                                        faculty_group=ctx.faculty_group)
+            request.session[SESSION_KEY] = course
+            self.decorate_request(request, course)
+            return None
 
         # When course_activation is turned on, use the MethCourseListView
         # which has the course activation feature instead of courseaffils'

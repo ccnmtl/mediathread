926a89ab077ab2a84f272b1e90f95e348539e67e
Anders Pearson <anders@columbia.edu>
2016-08-10 14:40:41 +0100

    django 1.10 style urls.py

--- mediathread/taxonomy/urls.py
+++ mediathread/taxonomy/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 import os.path
+
+from .views import taxonomy_workspace
 
 media_root = os.path.join(os.path.dirname(__file__), "media")
 
-urlpatterns = patterns(
-    'mediathread.taxonomy.views',
-
+urlpatterns = [
     url(r'^$',
-        'taxonomy_workspace',
+        taxonomy_workspace,
         name='taxonomy-workspace'),
 
     url(r'^/(?P<vocabulary_id>\d+)/$',
-        'taxonomy_workspace',
+        taxonomy_workspace,
         name='taxonomy-workspace-view')
-)
+]

e4a01f2128393ded5078bce63ad633ee60d20af0
Nik Nyby <nnyby@columbia.edu>
2016-11-04 16:21:51 -0400

    Add some basic tests for the sequence assignment API

--- mediathread/sequence/apiviews.py
+++ mediathread/sequence/apiviews.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from mediathread.sequence.models import (
     SequenceAsset, SequenceMediaElement, SequenceTextElement,
 )
@@ -8,16 +8,19 @@ from mediathread.sequence.serializers import (
 )
 
 
-class SequenceAssetViewSet(viewsets.ReadOnlyModelViewSet):
+class SequenceAssetViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = SequenceAsset.objects.all()
     serializer_class = SequenceAssetSerializer
 
 
 class SequenceMediaElementViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = SequenceMediaElement.objects.all()
     serializer_class = SequenceMediaElementSerializer
 
 
 class SequenceTextElementViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = SequenceTextElement.objects.all()
     serializer_class = SequenceTextElementSerializer

1d214b319d6ec71cb5e879262f8bc13bc4a01859
Nik Nyby <nnyby@columbia.edu>
2016-11-16 16:01:54 -0500

    add ProjectSequenceAsset api tests

--- mediathread/projects/apiviews.py
+++ mediathread/projects/apiviews.py
@@ -1,9 +1,16 @@
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
 from mediathread.projects.models import ProjectSequenceAsset
 from mediathread.projects.serializers import ProjectSequenceAssetSerializer
 
 
-class ProjectSequenceAssetViewSet(viewsets.ModelViewSet):
+class ProjectSequenceAssetViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
     queryset = ProjectSequenceAsset.objects.all()
     serializer_class = ProjectSequenceAssetSerializer
+
+    def list(self, request):
+        queryset = ProjectSequenceAsset.objects.filter(
+            sequence_asset__author=request.user)
+        serializer = ProjectSequenceAssetSerializer(queryset, many=True)
+        return Response(serializer.data)
--- mediathread/projects/serializers.py
+++ mediathread/projects/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from mediathread.projects.models import ProjectSequenceAsset
+from mediathread.sequence.serializers import SequenceAssetSerializer
 
 
 class ProjectSequenceAssetSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ProjectSequenceAsset
         fields = ('id', 'sequence_asset', 'project')
+
+    project = serializers.PrimaryKeyRelatedField(read_only=True)
+    sequence_asset = SequenceAssetSerializer()

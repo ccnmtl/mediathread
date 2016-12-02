ab49599e1701dab231eec11922979bc7d3bbc370
Nik Nyby <nnyby@columbia.edu>
2016-12-02 15:33:13 -0500

    Add unit test for update_track_elements

--- mediathread/sequence/tests/test_models.py
+++ mediathread/sequence/tests/test_models.py
@@ -21,6 +21,33 @@ class SequenceAssetTest(TestCase):
         self.assertEqual(SequenceMediaElement.objects.count(), 0)
         self.assertEqual(SequenceTextElement.objects.count(), 0)
 
+        media_track = []
+        for i in range(3):
+            e = SequenceMediaElementFactory(sequence_asset=self.asset)
+            media_track.append({
+                'start_time': i,
+                'end_time': i + 0.5,
+                'media': e.media,
+            })
+
+        text_track = []
+        for i in range(4):
+            e = SequenceTextElementFactory(sequence_asset=self.asset)
+            text_track.append({
+                'start_time': i,
+                'end_time': i + 0.5,
+                'text': 'text {}'.format(i),
+            })
+
+        self.asset.update_track_elements(media_track, text_track)
+        self.assertEqual(SequenceMediaElement.objects.count(), 3)
+        self.assertEqual(SequenceTextElement.objects.count(), 4)
+
+        el0 = SequenceTextElement.objects.filter(text='text 0').first()
+        self.assertEqual(el0.start_time, 0)
+        self.assertEqual(el0.end_time, 0.5)
+        self.assertEqual(el0.sequence_asset, self.asset)
+
 
 class SequenceMediaElementTest(TestCase):
     def setUp(self):

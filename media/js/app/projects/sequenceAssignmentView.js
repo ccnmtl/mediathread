2c87d2a6c92d6adb6b48a81e7bf0a2915b21715e
Susan Dreher <sdreher@columbia.edu>
2016-12-08 13:22:40 -0500

    SequenceAssignmentView: reflection space

--- media/js/app/projects/sequenceAssignmentView.js
+++ media/js/app/projects/sequenceAssignmentView.js
@@ -1,5 +1,6 @@
 /* global _: true, AssignmentView: true, updateUserSetting: true */
-/* global MediaThread: true */
+/* global MediaThread: true, tinymceSettings:true, tinymce: true */
+/* global showMessage: true */
 // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
 
 /**
@@ -19,15 +20,19 @@
             'click .btn-show-submit': 'onShowSubmitDialog',
             'click .toggle-feedback': 'onToggleFeedback',
             'click .save-feedback': 'onSaveFeedback',
-            'change textarea[name="comment"]': 'onChangeFeedback',
-            'keyup textarea[name="comment"]': 'onChangeFeedback',
-            'paste textarea[name="comment"]': 'onChangeFeedback'
+            'change textarea[name="comment"]': 'onChange',
+            'keyup textarea[name="comment"]': 'onChange',
+            'paste textarea[name="comment"]': 'onChange',
+            'keyup input[name="title"]': 'onChange',
+            'click .btn-save': 'onSaveProject'
         },
         initialize: function(options) {
             _.bindAll(this, 'render', 'onToggleFeedback',
                       'onShowSubmitDialog', 'onSubmitResponse',
                       'onSaveFeedback', 'onSaveFeedbackSuccess',
-                      'onChangeFeedback');
+                      'onChange', 'onSaveProject', 'serializeData',
+                      'isDirty', 'setDirty', 'beforeUnload',
+                      'validTitle');
 
             AssignmentView.prototype.initialize.apply(this, arguments);
 
@@ -39,11 +44,30 @@
             jQuery('#accordion').on('shown.bs.collapse', function() {
                 updateUserSetting(MediaThread.current_username, key, true);
             });
+
+            var self = this;
+            var settings = jQuery.extend(tinymceSettings, {
+                height: '300',
+                setup: function(ed) {
+                    ed.on('change', function(e) {
+                        self.setDirty(true);
+                    });
+                }
+            });
+            tinymce.init(settings);
+        },
+        beforeUnload: function() {
+            // Check tinymce dirty state.
+            // For some reason, the instance is not always current
+            if (this.isDirty()) {
+                return 'Changes to your project have not been saved.';
+            }
         },
         readyToSubmit: function() {
             return true;
         },
-        onChangeFeedback: function() {
+        onChange: function() {
+            this.setDirty(true);
             this.$el.find('.alert-success').fadeOut();
         },
         onSaveFeedbackSuccess: function(frm, json) {
@@ -51,6 +75,73 @@
             var today = Date().toLocaleString();
             var dataId = 'feedback-date-' + this.responseId;
             this.$el.find('span[data-id=' + dataId + ']').html(today);
+        },
+        serializeData: function() {
+            var q = '[name="title"], [name="body"]';
+            return this.$el.find(q).serializeArray();
+        },
+        isDirty: function() {
+            return tinymce.activeEditor.isDirty();
+        },
+        setDirty: function(isDirty) {
+            var $elt = this.$el.find('.btn-save');
+
+            if (isDirty) {
+                $elt.text('Save');
+            } else {
+                tinymce.activeEditor.isNotDirty = true;
+                $elt.text('Saved');
+            }
+        },
+        validTitle: function() {
+            var $title = this.$el.find('input.project-title').first();
+            var value = $title.val();
+            if (!value || value.length < 1) {
+                showMessage(
+                    'Please specify a title for your response',
+                    null, 'Error');
+                $title.focus();
+                return false;
+            }
+            return true;
+        },
+        onSaveProject: function() {
+            tinymce.activeEditor.save();
+
+            if (!this.validTitle()) {
+                return false;
+            }
+
+            var $saveButton = this.$el.find('.btn-save');
+            $saveButton.attr('disabled', 'disabled')
+                .text('Saving...')
+                .addClass('saving');
+
+            var self = this;
+            jQuery.ajax({
+                type: 'POST',
+                url: '/project/save/' + this.responseId + '/',
+                data: this.serializeData(),
+                dataType: 'json',
+                error: function() {
+                    $saveButton.removeAttr('disabled')
+                        .text('Save').removeClass('saving');
+                    showMessage('There was an error saving your project.',
+                                null, 'Error');
+                },
+                success: function(json, textStatus, xhr) {
+                    if (json.status === 'error') {
+                        showMessage(json.msg, null, 'Error');
+                    } else {
+                        self.setDirty(false);
+                    }
+                    $saveButton.removeAttr('disabled')
+                        .removeClass('saving', 1200, function() {
+                            jQuery(self).text('Saved'); });
+                }
+            });
+
+            return true;
         }
     });
 }(jQuery));

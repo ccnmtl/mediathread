46dc8fd5c27fe5f24ebafc150fa035c5533b2c14
Susan Dreher <sdreher@columbia.edu>
2017-01-12 11:41:03 -0500

    Convert FLVs from migrated sources

--- mediathread/main/management/commands/update_migrated_flv.py
+++ mediathread/main/management/commands/update_migrated_flv.py
@@ -5,13 +5,22 @@ from mediathread.assetmgr.models import Source
 
 class Command(BaseCommand):
 
+    help = 'Migrate flvs with an mp4 match'
+
+    def add_arguments(self, parser):
+        parser.add_argument('-f', '--fake', action='store_true')
+
     def handle(self, *app_labels, **options):
+
         # For each flv in the system, see if there is an mp4_pseudo match
-        # Match occurs very simply by title
+        # Match occurs very simply by title. There is a risk of overlap
+        # but a quick review of videos reveals this to be okay
+        uploaded = 'Mediathread video uploaded by'
         sources = Source.objects.filter(
             primary=True, label__in=['flv_pseudo', 'flv']).exclude(
-                asset__title__isnull=True).exclude(asset__title__exact='')
-        print 'Found {} sources'.format(sources.count())
+                asset__title__isnull=True).exclude(
+                asset__title__exact='').exclude(
+                asset__title__startswith=uploaded).select_related('asset')
 
         n = 1
         for source in sources:
@@ -21,10 +30,17 @@ class Command(BaseCommand):
                 url__isnull=True).exclude(url__exact='').first()
 
             if mp4_source:
-                print '{}, {}, {}, {}, {}'.format(
-                    source.asset.title, source.asset.id, mp4_source.asset.id,
+                msg = '{}, {}, {}, {}, {}, {}, {}'.format(
+                    source.asset.title,
+                    source.asset.course.title.encode('utf-8').strip(),
+                    source.asset.id,
+                    mp4_source.asset.course.title.encode('utf-8').strip(),
+                    mp4_source.asset.id,
                     source.url, mp4_source.url)
+                print msg
+
+                if not options['fake']:
+                    source.asset.update_primary('mp4_pseudo', mp4_source.url)
                 n += 1
-                #source.asset.update_primary('mp4_pseudo', mp4_source.url)
 
         print 'Updated {}'.format(n)

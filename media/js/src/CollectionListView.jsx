98da8bf2ba12f830f1946049946435a07b39239f
Nikolas Nyby <nikolas@gnu.org>
2020-04-21 14:47:25 -0400

    Integrate Marc's new list view design
    Still TODO here: new functionality like adding type, parent, tags and
    terms.

--- media/js/src/CollectionListView.jsx
+++ media/js/src/CollectionListView.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import DataTable from 'react-data-table-component';
+import DataTable, { createTheme } from 'react-data-table-component';
 import NoAssetsFound from './NoAssetsFound';
 
 export default class CollectionListView extends React.Component {
@@ -10,30 +10,83 @@ export default class CollectionListView extends React.Component {
         } else {
             const columns = [
                 {
-                    name: 'Item Title',
+                    name: 'Title',
                     selector: 'title',
                     sortable: true
                 },
                 {
-                    name: 'Item Type',
+                    name: 'Tags',
+                    selector: 'tags',
+                    sortable: true
+                },
+                {
+                    name: 'Terms',
+                    selector: 'terms',
+                    sortable: true
+                },
+                {
+                    name: 'Media',
                     selector: 'primary_type',
                     sortable: true
                 },
                 {
-                    name: 'Item Owner',
+                    name: 'Owner',
                     selector: 'author.public_name',
                     sortable: true
                 },
                 {
-                    name: 'Item Date',
+                    name: 'Date',
                     selector: 'modified',
                     sortable: true
                 }
             ];
+
+            createTheme('mediathread', {
+                striped: {
+                    default: 'rgba(0, 0, 0, 0.05)'
+                }
+            });
+
+            const styles = {
+                header: {
+                    style: {
+                        minHeight: 0
+                    }
+                },
+                headRow: {
+                    style: {
+                        minHeight: '34px'
+                    }
+                },
+                headCells: {
+                    style: {
+                        color: 'rgba(33, 37, 41)',
+                        fontSize: '16px',
+                        fontWeight: 700,
+                        backgroundColor: '#ccc',
+                        borderColor: '#ccc',
+                        padding: '0.3rem'
+                    }
+                },
+                rows: {
+                    style: {
+                        fontSize: '16px'
+                    }
+                },
+                cells: {
+                    style: {
+                        padding: '0.3rem'
+                    }
+                }
+            };
+
             return (
                 <DataTable
-                    title="Assets"
+                    theme="mediathread"
+                    customStyles={styles}
                     columns={columns}
+                    highlightOnHover={true}
+                    striped={true}
                     data={this.props.assetList} />
             );
         }

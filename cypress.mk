bc6a89804f79fcf258110bad2ac76a3252d5d608
Nikolas Nyby <nikolas@gnu.org>
2020-04-27 17:55:52 -0400

    Cypress makefile: remove redundant js_sentinel definition
    This was causing a warning.

--- cypress.mk
+++ cypress.mk
@@ -1,13 +1,3 @@
-JS_FILES ?= media/js
-
-NODE_MODULES ?= ./node_modules
-JS_SENTINAL ?= $(NODE_MODULES)/sentinal
-
-$(JS_SENTINAL): package.json
-	rm -rf $(NODE_MODULES)
-	npm install
-	touch $(JS_SENTINAL)
-
 cypress: $(JS_SENTINAL)
 	npm run cypress:test
 

26d1f7c4b1f2e3f3dee313363cd6418bf78a137c
ndittren <36773036+ndittren@users.noreply.github.com>
2020-06-04 02:35:10 -0400

    Scenario 1 tests  plus some structure changes

---
+++ cypress/integration/course/course.js
@@ -0,0 +1,7 @@
+// describe('Course Navigation', function() {
+//     it('Logs in as instructor_one and navigates to a course', function() {
+//         cy.login('instructor_one', 'test');
+//         cy.visit('/course/1/');
+//         cy.url().should('match', /course\/1\/$/);
+//     });
+// });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -35,3 +35,15 @@ Cypress.Commands.add('login', (username, password) => {
      },
    });
  });
+
+Cypress.Commands.add('getIframeBody', () => {
+  // get the iframe > document > body
+  // and retry until the body element is not empty
+  return cy
+  .get('iframe')
+  .its('0.contentDocument.body').should('not.be.empty')
+  // wraps "body" DOM element to allow
+  // chaining more Cypress commands, like ".find(...)"
+  // https://on.cypress.io/wrap
+  .then(cy.wrap);
+});

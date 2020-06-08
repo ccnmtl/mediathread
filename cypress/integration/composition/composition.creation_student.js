88aea3f97b140f736f7a6a9b7c26c3cf2efec711
ndittren <36773036+ndittren@users.noreply.github.com>
2020-06-07 22:00:25 -0400

    composition tests

--- cypress/integration/composition/composition.creation_student.js
+++ cypress/integration/composition/composition.creation_student.js
@@ -9,59 +9,51 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 describe('Student Creates Composition', () => {
 
     beforeEach(() => {
-        cy.login('student_one', 'test');
-        cy.visit('/course/1/');
+      cy.login('student_one', 'test');
+      cy.visit('/course/1/');
     });
 
     it('should check composition panel edit features', () => {
-        cy.get('#homepage-create-menu').should('exist').click();
-        cy.get('#create-project-menu input[type="submit"]')
-          .contains('Create Composition').click();
-        cy.get('#loaded').should('exist');
-        cy.get('.panhandle-stripe.composition').should('exist');
-        cy.get('.panel-subcontainer-title').contains('Untitled').should('exist');
-        cy.contains('ul', 'Student One').should('exist');
-        cy.get('.project-visibility-description').contains('Draft').should('exist');
-        cy.get('td.panel-container.open.composition').should('exist');
-        cy.get('.project-revisionbutton').should('exist');
-        cy.get('.project-previewbutton').should('exist');
-        cy.contains('Edit').should('not.exist');
-        cy.get('.project-savebutton').should('exist');
-        cy.get('.participant_list').contains('Authors').should('exist');
+      cy.get('#homepage-create-menu').should('exist').click();
+      cy.get('#create-project-menu input[type="submit"]')
+        .contains('Create Composition').click();
+      cy.get('#loaded').should('exist');
+      cy.get('.panhandle-stripe.composition').should('exist');
+      cy.get('.panel-subcontainer-title').contains('Untitled').should('exist');
+      cy.contains('ul', 'Student One').should('exist');
+      cy.get('.project-visibility-description').contains('Draft').should('exist');
+      cy.get('td.panel-container.open.composition').should('exist');
+      cy.get('.project-revisionbutton').should('exist');
+      cy.get('.project-previewbutton').should('exist');
+      cy.contains('Edit').should('not.exist');
+      cy.get('.project-savebutton').should('exist');
+      cy.get('.participant_list').contains('Authors').should('exist');
     });
     it('should save a composition', () => {
-        cy.visit('/project/view/1');
-        cy.get('.panel-subcontainer-title > .form-control').clear()
-          .type('Composition: Scenario 2');
-        cy.get('.project-savebutton').click();
-        cy.get('#id_publish').find('li')
-          .should('contain', 'Draft - only you can view')
-        cy.get('input[name=publish]:checked').should('exist');
-        cy.get('#id_publish').find('li')
-          .should('contain', 'Whole Class - all class members can view');
-        cy.get('#id_publish').find('li')
-          .should('not.contain', 'Whole World - a public url is provided');
-        cy.get('.btn-primary').contains('Save').click();
-        cy.get('.project-visibility-link').should('exist');
-        cy.get('.project-savebutton').should('contain', 'Saved');
+      cy.visit('/project/view/2/');
+      cy.get('.panel-subcontainer-title > .form-control').clear()
+        .type('Composition: Scenario 2');
+      cy.getIframeBody().find('p').click()
+        .type('The Columbia Center for New Teaching and Learning');
+      cy.get('.project-savebutton').click();
+      cy.get('.btn-primary').contains('Save').click();
+      cy.get('.project-savebutton').should('contain', 'Saved');
     });
-    // it('should toggle the preview mode', () => {
-    //     cy.visit('/project/view/1');
-    //     cy.wait(400);
-    //     cy.get('.project-previewbutton').click();
-    //     cy.get('.project-revisionbutton').should('exist');
-    //     cy.contains('Edit').should('exist');
-    //     cy.contains('Preview').should('not.exist');
-    //     cy.get('.project-savebutton').should('contain', 'Saved');
-    //     cy.get('.participant_list').should('not.be', 'visable');
-    // });
-    // it('should show on Home', () => {
-    //     cy.visit('/');
-    //     cy.get('#course_title_link').should('exist').click();
-    //     cy.get('#loaded').should('exist');
-    //     cy.get('li.projectlist').its('length').should('be.gt', 0);
-    //     cy.get('.asset_title').should('contain', 'Composition: Scenario 1');
-    //     cy.get('.metadata-value-author').should('contain', 'Instructor One');
-    // })
-
+    it('should toggle the preview mode', () => {
+      cy.visit('/project/view/2');
+      cy.get('.project-previewbutton').trigger('mouseover').click();
+      cy.get('.project-revisionbutton').should('exist');
+      cy.contains('Edit').should('exist');
+      cy.contains('Preview').should('not.exist');
+      cy.get('.project-savebutton').should('contain', 'Saved');
+      cy.get('.participant_list').should('not.be', 'visable');
+    });
+    it('should show on Home', () => {
+      cy.visit('/');
+      cy.get('#course_title_link').should('exist').click();
+      cy.get('#loaded').should('exist');
+      cy.get('li.projectlist').its('length').should('be.gt', 0);
+      cy.get('.asset_title').should('contain', 'Composition: Scenario 2');
+      cy.get('.metadata-value-author').should('contain', 'Student One');
+    });
 });
---
+++ cypress/integration/composition/composition.visibility.js
@@ -0,0 +1,61 @@
+// TODO: why are we getting an error for `the_records`
+// here in the course settings page?
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false;
+});
+
+describe('Student Project Visibility', () => {
+
+    it('creates a project as an Instructor', () => {
+      cy.login('instructor_one', 'test');
+      cy.visit('/course/1/');
+      cy.get('#homepage-create-menu').click();
+      cy.get('#create-project-menu input[type="submit"]')
+        .eq(1).contains('Create Composition').click();
+      cy.get('#loaded').should('exist');
+      cy.get('.panel-subcontainer-title > .form-control').clear()
+        .type('Composition Visibility: Scenario 3');
+      cy.get('.project-savebutton').click();
+      cy.contains('Whole Class - all class members can view').click();
+      cy.get('.btn-primary').contains('Save').click();
+      cy.get('.project-savebutton').should('contain', 'Saved');
+      cy.get('.project-visibility-link').should('contain', 'Published to Class');
+    });
+    it('views composition as a Student', () => {
+      cy.login('student_one', 'test');
+      cy.visit('/course/1/');
+      cy.get('.instructor-list')
+        .should('have.length', 1)
+        .and('contain', 'Composition Visibility: Scenario 3');
+      cy.get('.switcher-top').click();
+      cy.get('#choice_all_items > .switcher-choice').click();
+      cy.get('.projectlist').should('exist')
+        .and('contain', 'Composition Visibility: Scenario 3');
+    });
+    it('creates a project as Student one', () => {
+      cy.login('student_one', 'test');
+      cy.visit('/course/1/');
+      cy.get('#homepage-create-menu').click();
+      cy.get('#create-project-menu input[type="submit"]')
+        .contains('Create Composition').click();
+      cy.get('#loaded').should('exist');
+      cy.get('.panel-subcontainer-title > .form-control').clear()
+        .type('Visibility');
+      cy.get('.project-savebutton').click();
+      cy.contains('Whole Class - all class members can view').click();
+      cy.get('.btn-primary').contains('Save').click();
+      cy.get('.project-savebutton').should('contain', 'Saved');
+      cy.get('.project-visibility-link').should('contain', 'Published to Class');
+    });
+    it('views Student One composition as Student Two', () => {
+      cy.login('student_two', 'test');
+      cy.visit('/course/1/');
+      cy.get('.switcher-top').click();
+      cy.get('.switcher-choice.owner').contains('One, Student').click();
+      cy.get('.metadata-value-author').contains('Student One');
+      cy.get('.projectlist').should('exist')
+        .and('contain', 'Visibility');
+    })
+});

06a6f8c3006e66587a7202fa1b2748fea00b8375
Nikolas Nyby <nikolas@gnu.org>
2020-06-16 16:59:41 -0400

    Add some cypress waits
    These tests are unstable and causing master to fail. I'm adding in some
    waits to see if this improves things.

--- cypress/integration/3.assignments/assignment.response_student.js
+++ cypress/integration/3.assignments/assignment.response_student.js
@@ -9,6 +9,7 @@ describe('Assignment Feature: Student Response', () => {
     beforeEach(() => {
         cy.login('student_one', 'test');
         cy.visit('/project/view/1');
+        cy.wait(500);
         //TODO: Test navigation to sample project from new Assignments tab
     });
 

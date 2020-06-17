a3b61c9151634b5f757d592ba64ee42b418e8dea
Nikolas Nyby <nikolas@gnu.org>
2020-06-17 14:53:33 -0400

    Add asset references display

--- media/js/src/assetDetail/ViewItem.jsx
+++ media/js/src/assetDetail/ViewItem.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Alert from 'react-bootstrap/Alert';
+
+import {getAssetReferences} from '../utils';
+
 export default class ViewItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            references: null
+        };
+
+        const me = this;
+        getAssetReferences(this.props.asset.id).then(function(data) {
+            if ('references' in data) {
+                me.setState({references: data['references']});
+            }
+        });
+    }
     render() {
+        const references = [];
+        if (this.state.references) {
+            this.state.references.forEach(function(reference, idx) {
+                references.push(
+                    <div className="card w-100 card-selection" key={idx}>
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                <div>
+                                    {reference.title}
+                                </div>
+                            </h5>
+                            <h6>
+                                Assignment
+                            </h6>
+                            <p className="card-text">
+                                Collected 12/19/15 01:02 AM
+                            </p>
+                            <a href="#" className="btn btn-primary btn-sm">
+                                View
+                            </a>
+                        </div>
+                    </div>
+                );
+            });
+        }
+
         return (
             <div className="tab-content" id="pills-tabContent">
                 <h3>
@@ -23,11 +66,16 @@ export default class ViewItem extends React.Component {
                         <a className="dropdown-item" href="#">
                             Title, Z &ndash; A
                         </a>
-                        <a className="dropdown-item" href="#">
-                            Something else
-                        </a>
                     </div>
                 </div>
+
+                <Alert variant="warning" classNameshow={
+                    this.state.references && this.state.references.length === 0
+                }>
+                    There are no references in this course.
+                </Alert>
+
+                {references}
             </div>
         );
 

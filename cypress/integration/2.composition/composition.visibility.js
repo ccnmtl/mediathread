83d48f4d783b4037bfb47cbfd2fc05d082ce8492
Susan Dreher <sdreher@gmail.com>
2020-07-25 16:23:38 -0400

    revert one more test

--- cypress/integration/2.composition/composition.visibility.js
+++ cypress/integration/2.composition/composition.visibility.js
@@ -39,7 +39,7 @@ describe('Student Project Visibility', () => {
          cy.visit('/course/1/projects/');
          cy.get('#cu-privacy-notice-icon').click();
          cy.get('#select-owner').select('instructor_one');
-         cy.contains('Composition Public: Scenario 3').parent('td').parent('tr').within(() => {
+         cy.contains('Composition Public: Scenario 3').parent('tr').within(() => {
              // all searches are automatically rooted to the found tr element
              cy.get('td').eq(0).contains('Composition Public: Scenario 3');
              cy.get('td').eq(1).contains('Shared with Class');
@@ -74,7 +74,7 @@ describe('Student Project Visibility', () => {
          cy.get('#cu-privacy-notice-icon').click();
          cy.get('#select-owner').select('student_one');
 
-         cy.contains('Student One Public Essay').parent('td').parent('tr').within(() => {
+         cy.contains('Student One Public Essay').parent('tr').within(() => {
              // all searches are automatically rooted to the found tr element
              cy.get('td').eq(0).contains('Student One Public Essay');
              cy.get('td').eq(1).contains('Shared with Class');

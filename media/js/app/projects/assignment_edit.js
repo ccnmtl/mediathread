ea6eb4187db7f4a3381a40239d9e49e8b7d1d2bd
Susan Dreher <sdreher@gmail.com>
2020-07-28 08:46:50 -0400

    Revert "Composition Assignment Wizard: Remove visibility options"

--- media/js/app/projects/assignment_edit.js
+++ media/js/app/projects/assignment_edit.js
@@ -43,15 +43,9 @@
                 return title.length > 0 && body.length > 0;
             } else if (pageContent === 'due-date') {
                 var q1 = 'input[name="due_date"]';
-                if (jQuery(q1).val() === undefined || jQuery(q1).val() === '')
-                    return false;
-
-                var q2 = 'input[name="response_view_policy"]';
-                if (jQuery(q2).length &&
-                        jQuery(q2 + ':checked').val() === undefined) {
-                    return false;
-                }
-                return true;
+                var q2 = 'input[name="response_view_policy"]:checked';
+                return jQuery(q1).val() !== undefined &&
+                    jQuery(q1).val() !== '' && jQuery(q2).val() !== undefined;
             } else if (pageContent === 'publish') {
                 var q = 'input[name="publish"]:checked';
                 return jQuery(q).val() !== undefined;

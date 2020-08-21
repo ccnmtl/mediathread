809b1254c65e0de2a5ac1996b9619b5ab3ecb5ec
Nikolas Nyby <nikolas@gnu.org>
2020-08-21 13:22:18 -0400

    Re-organize Item Metadata view - MEDT-3631

--- media/js/src/assetDetail/ViewItem.jsx
+++ media/js/src/assetDetail/ViewItem.jsx
@@ -3,9 +3,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Alert from 'react-bootstrap/Alert';
+import find from 'lodash/find';
 
-import {getAssetReferences} from '../utils';
+import {getAssetReferences, getTags, getTerms} from '../utils';
 
 export default class ViewItem extends React.Component {
     constructor(props) {
@@ -22,8 +22,6 @@ export default class ViewItem extends React.Component {
         });
     }
     render() {
-        const references = [];
-
         let authorId = null;
         if (this.props.asset && this.props.asset.author) {
             authorId = this.props.asset.author.id;
@@ -34,34 +32,73 @@ export default class ViewItem extends React.Component {
             userIsAuthor = authorId === window.MediaThread.current_user;
         }
 
+        const references = [];
         if (this.state.references) {
             this.state.references.forEach(function(reference, idx) {
                 references.push(
-                    <div className="card w-100 card-selection" key={idx}>
-                        <div className="card-body">
-                            <h5 className="card-title">
-                                <div>
-                                    {reference.title}
-                                </div>
-                            </h5>
-                            <h6>
-                                Assignment
-                            </h6>
-                            <p className="card-text">
-                                Collected 12/19/15 01:02 AM
-                            </p>
-                            <a href="#" className="btn btn-primary btn-sm">
-                                View
-                            </a>
-                        </div>
-                    </div>
+                    <li key={idx}>
+                        <a href={reference.url}>{reference.title}</a>
+                    </li>
                 );
             });
         }
 
+        let tags = 'There are no tags.';
+        let terms = 'There are no terms.';
+        if (this.props.asset && this.props.asset.annotations) {
+            const tagsArray = getTags(this.props.asset.annotations);
+            if (tagsArray.length > 0) {
+                tags = tagsArray.join(', ');
+            }
+
+            const termsArray = getTerms(this.props.asset.annotations);
+            if (termsArray.length > 0) {
+                terms = termsArray.join(', ');
+            }
+        }
+
+        let description = null;
+        if (this.props.asset && this.props.asset.metadata) {
+            let desc = find(this.props.asset.metadata, {key: 'Description'});
+            if (desc) {
+                desc = desc.value;
+            }
+            if (desc && desc.length) {
+                description = desc[0];
+            }
+        }
+
         return (
             <div className="tab-content" id="pills-tabContent">
-                <table className="table mt-1">
+                <h3>Tags</h3>
+                <p>
+                    {tags}
+                </p>
+
+                <hr />
+
+                <h3>Terms</h3>
+                <p>
+                    {terms}
+                </p>
+
+                <hr />
+
+                <h3>Course References</h3>
+                {this.state.references && this.state.references.length === 0 && (
+                    <div>There are no references in this course.</div>
+                )}
+
+                {this.state.references && this.state.references.length > 0 && (
+                    <ul>
+                        {references}
+                    </ul>
+                )}
+
+                <hr />
+
+                <h3>Additional Information (Metadata)</h3>
+                <table className="table table-sm table-borderless mt-1 table-metadata">
                     <tbody>
                         <tr>
                             <th scope="row">Item Name</th>
@@ -92,38 +129,13 @@ export default class ViewItem extends React.Component {
                     </tbody>
                 </table>
 
-                <button type="submit" className="btn btn-danger btn-sm mb-2">
+                <p>
+                    {description}
+                </p>
+
+                <button type="submit" className="btn btn-danger btn-sm float-right">
                     Remove from my Collection
                 </button>
-
-                <h4>
-                    Item References within Course
-                </h4>
-
-                <div className="btn-group">
-                    <a
-                        className="btn btn-light dropdown-toggle"
-                        data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false">
-                        Sort by
-                    </a>
-                    <div className="dropdown-menu">
-                        <a className="dropdown-item" href="#">
-                            Title, A &ndash; Z
-                        </a>
-                        <a className="dropdown-item" href="#">
-                            Title, Z &ndash; A
-                        </a>
-                    </div>
-                </div>
-
-                <Alert variant="warning" show={
-                    this.state.references && this.state.references.length === 0
-                }>
-                    There are no references in this course.
-                </Alert>
-
-                {references}
             </div>
         );
 

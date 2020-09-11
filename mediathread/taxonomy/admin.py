0c48e1d25999ac32875afcba272f8eb1237711e3
Susan Dreher <sdreher@gmail.com>
2020-09-11 18:09:35 -0400

    Quick fix for the taxonomy admin

--- mediathread/taxonomy/admin.py
+++ mediathread/taxonomy/admin.py
@@ -10,32 +10,17 @@ def term_vocabulary_name(obj):
 term_vocabulary_name.short_description = 'Vocabulary'
 
 
-def term_vocabulary_related_name(obj):
-    return smart_text(obj.vocabulary.content_object)
-
-
-term_vocabulary_related_name.short_description = 'Related To'
-
-
 class TermAdmin(admin.ModelAdmin):
     class Meta:
         model = Term
 
     search_fields = ("display_name", "vocabulary__display_name")
     list_display = ("display_name",
-                    term_vocabulary_name,
-                    term_vocabulary_related_name)
+                    term_vocabulary_name)
 
 
 admin.site.register(Term, TermAdmin)
 admin.site.register(TermRelationship)
-
-
-def vocabulary_related_name(obj):
-    return smart_text(obj.content_object)
-
-
-vocabulary_related_name.short_description = 'Related To'
 
 
 class VocabularyAdmin(admin.ModelAdmin):
@@ -43,7 +28,7 @@ class VocabularyAdmin(admin.ModelAdmin):
         model = Vocabulary
 
     search_fields = ("display_name",)
-    list_display = ("display_name", vocabulary_related_name)
+    list_display = ("display_name", "course")
 
 
 admin.site.register(Vocabulary, VocabularyAdmin)

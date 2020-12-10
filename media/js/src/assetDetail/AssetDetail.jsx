da1dbd3080197c7e7c0ee4b8db6fb6b81152bb58
ndittren <36773036+ndittren@users.noreply.github.com>
2020-12-10 11:55:21 -0500

    wip

--- media/js/src/assetDetail/AssetDetail.jsx
+++ media/js/src/assetDetail/AssetDetail.jsx
@@ -99,6 +99,7 @@ export default class AssetDetail extends React.Component {
 
         this.playerRef = React.createRef();
         this.polygonButtonRef = React.createRef();
+        this.freeformButtonRef = React.createRef();
         this.startButtonRef = React.createRef();
 
         this.asset = new Asset(this.props.asset);
@@ -133,6 +134,7 @@ export default class AssetDetail extends React.Component {
         this.onClearVectorLayer = this.onClearVectorLayer.bind(this);
 
         this.addInteraction = this.addInteraction.bind(this);
+        this.addFFInteraction = this.addFFInteraction.bind(this);
         this.onUpdateAssetTitle = this.onUpdateAssetTitle.bind(this);
     }
 
@@ -560,6 +562,15 @@ export default class AssetDetail extends React.Component {
                                     </button>
                                     <button
                                         type="button"
+                                        ref={this.freeformButtonRef}
+                                        className="btn btn-light btn-sm mr-2 polygon-button"
+                                        onClick={this.addFFInteraction}>
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fillRule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                                        </svg> Free Form
+                                    </button>
+                                    <button
+                                        type="button"
                                         className="btn btn-light btn-sm"
                                         onClick={this.onClearVectorLayer}>
                                         Clear
@@ -940,6 +951,28 @@ export default class AssetDetail extends React.Component {
             source: this.selectionSource,
             stopClick: true,
             type: 'Polygon'
+        });
+
+        // Every time a drawing is started, clear the vector
+        // layer. Each selection only has a single shape, for now.
+        this.draw.on('drawstart', this.onClearVectorLayer);
+        this.draw.on('drawend', this.onDrawEnd);
+        this.draw.on('drawabort', this.onDrawEnd);
+
+        this.map.addInteraction(this.draw);
+    }
+
+    addFFInteraction() {
+        if (this.draw) {
+            this.map.removeInteraction(this.draw);
+        }
+
+        this.setState({isDrawing: true});
+
+        this.draw = new Draw({
+            source: this.selectionSource,
+            type: 'LineString',
+            freehand: true,
         });
 
         // Every time a drawing is started, clear the vector

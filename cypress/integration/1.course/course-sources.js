e90cfca07e4ee521fa629d3c8851922c1b6fef76
ndittren <36773036+ndittren@users.noreply.github.com>
2021-05-05 15:04:52 -0400

    Moved the sources tests to one area

--- cypress/integration/1.course/course-sources.js
+++ cypress/integration/1.course/course-sources.js
@@ -3,12 +3,12 @@ describe('Instructor Course Sources', () => {
     before(() => {
         cy.login('instructor_one', 'test');
         cy.visit('/course/1/');
-        cy.get('a[href*="settings"]').click();
-        cy.get('a[href*="sources"]').click();
     });
 
     it('should check Sources page functionality', () => {
         cy.log('should navigate to Sources page');
+        cy.get('a[href*="settings"]').click();
+        cy.get('a[href*="sources"]').click();
         cy.url().should('match', /course\/1\/dashboard\/sources\/$/);
 
         cy.log('should add YouTube as a source to the class');

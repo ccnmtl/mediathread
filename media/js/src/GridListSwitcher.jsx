7e64fc1c443a91391c9720932f0f3423e224f307
ndittren <36773036+ndittren@users.noreply.github.com>
2022-09-15 16:06:26 -0400

    unique ids for view toggles

--- media/js/src/GridListSwitcher.jsx
+++ media/js/src/GridListSwitcher.jsx
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types';
 
 export default class GridListSwitcher extends React.Component {
     render() {
+        const uniqueId = (Math.random() + 1).toString(36).substring(7);
         return (
             <div className="col-md-6">
                 <div
-                    id="view-toggle" className="btn-group"
+                    id={`view-toggle=${uniqueId}`} className="btn-group"
                     role="group" aria-label="View Toggle">
                     <button
                         type="button"
-                        id="viewtoggle-grid"
+                        id={`viewtoggle-grid-${uniqueId}`}
                         data-testid="viewtoggle-grid"
                         onClick={() => this.props.setViewMode('grid')}
                         className={'btn btn-outline-primary btn-sm ' + (
@@ -20,7 +21,7 @@ export default class GridListSwitcher extends React.Component {
                     </button>
                     <button
                         type="button"
-                        id="viewtoggle-list"
+                        id={`viewtoggle-list-${uniqueId}`}
                         data-testid="viewtoggle-list"
                         onClick={() => this.props.setViewMode('list')}
                         className={'btn btn-outline-primary btn-sm ' + (

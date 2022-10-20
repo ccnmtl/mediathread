b7085227b0c06d17cfd4d5da1fd56bbfe7b62f15
ndittren <36773036+ndittren@users.noreply.github.com>
2022-10-20 13:57:37 -0400

    Add sorting

--- media/js/src/CollectionListView.jsx
+++ media/js/src/CollectionListView.jsx
@@ -12,9 +12,12 @@ import NoAssetsFound from './alerts/NoAssetsFound';
 import {
     formatDay, getAssetType, getAssetUrl, getTerms
 } from './utils';
+import Box from '@mui/material/Box';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import { visuallyHidden } from '@mui/utils';
 
-function createDate(name, selection, tags, vocabulary, media, owner, date) {
-    return { name, selection, tags, vocabulary, media, owner, date };
+function createData(title, selections, tags, vocabulary, media, owner, date) {
+    return { title, selections, tags, vocabulary, media, owner, date };
 }
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -33,16 +36,143 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
     '&:last-child td, &:last-child th': {
         border: 1,
         margin: 1
-    },
+    }
 }));
+
+function descendingComparator(a, b, orderBy) {
+    if(orderBy === 'title') {
+        if (b[orderBy].props.children < a[orderBy].props.children) {
+            return -1;
+        }
+        if (b[orderBy].props.children > a[orderBy].props.children) {
+            return 1;
+        }
+    } else {
+        if (b[orderBy] < a[orderBy]) {
+            return -1;
+        }
+        if (b[orderBy] > a[orderBy]) {
+            return 1;
+        }
+        return 0;
+    }
+}
+
+function getComparator(order, orderBy) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+
+const headCells = [
+    {
+        id: 'title',
+        align: 'left',
+        disablePadding: false,
+        label: 'Title',
+    },
+    {
+        id: 'selections',
+        align: 'right',
+        disablePadding: true,
+        label: 'Selections',
+    },
+    {
+        id: 'tags',
+        align: 'right',
+        disablePadding: false,
+        label: 'Tags',
+    },
+    {
+        id: 'vocabulary',
+        align: 'right',
+        disablePadding: true,
+        label: 'Course Vocabulary',
+    },
+    {
+        id: 'media',
+        align: 'right',
+        disablePadding: false,
+        label: 'Media',
+    },
+    {
+        id: 'owner',
+        align: 'right',
+        disablePadding: false,
+        label: 'Owner',
+    },
+    {
+        id: 'date',
+        align: 'right',
+        disablePadding: false,
+        label: 'Date',
+    },
+];
+
+function EnhancedTableHead(props) {
+    const {order, orderBy, onRequestSort } =
+      props;
+    const createSortHandler = (property) => (event) => {
+        onRequestSort(event, property);
+    };
+
+    return (
+        <TableHead>
+            <TableRow>
+                {headCells.map((headCell) => (
+                    <StyledTableCell
+                        key={headCell.id}
+                        align={headCell.align}
+                        padding={headCell.disablePadding ? 'none' : 'normal'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <Box component="span" sx={visuallyHidden}>
+                                    {order === 'desc' ?
+                                        'sorted descending' :
+                                        'sorted ascending'}
+                                </Box>
+                            ) : null}
+                        </TableSortLabel>
+                    </StyledTableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    onRequestSort: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired
+};
 
 export default class CollectionListView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading: false
+            loading: false,
+            order: 'asc',
+            orderBy: 'title',
         };
+        this.handleRequestSort = this.handleRequestSort.bind(this);
     }
+    handleRequestSort(event, property) {
+        const isAsc =
+                this.state.orderBy === property && this.state.order === 'asc';
+        this.setState({
+            order: isAsc ? 'desc' : 'asc',
+            orderBy: property
+        });
+    }
+
     render() {
         if (this.props.assets.length === 0) {
             return <NoAssetsFound />;
@@ -51,78 +181,72 @@ export default class CollectionListView extends React.Component {
         const me = this;
 
         const rows = [];
-
         me.props.assets.map((asset, index) => {
-            let name = <a
+            let title = <a
                 href={getAssetUrl(asset.id)}
                 key={index}
                 onClick={(e) => me.props.enterAssetDetailView(e, asset)}>
                 {asset.title}
             </a>;
-            let selection = asset.annotation_count;
+            let selections = asset.annotation_count;
             let tags = asset.tags.join(', ');
             let media = getAssetType(asset.primary_type);
             let owner = asset.author.public_name;
             let date = formatDay(asset);
             let vocabulary = getTerms(asset.annotations).join(', ');
-            rows.push(createDate(
-                name, selection, tags, vocabulary, media, owner, date));
+            rows.push(createData(
+                title, selections, tags, vocabulary, media, owner, date));
         });
 
         return (
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <StyledTableCell>Title</StyledTableCell>
-                            <StyledTableCell align="right">
-                                Selections
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                Tags
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                Course Vocabulary
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                Media
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                Owner
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                Date
-                            </StyledTableCell>
-                        </TableRow>
-                    </TableHead>
+                    <EnhancedTableHead
+                        order={this.state.order}
+                        orderBy={this.state.orderBy}
+                        onRequestSort={this.handleRequestSort}
+                    />
                     <TableBody>
-                        {rows.map((row) => (
-                            <StyledTableRow
-                                key={row.name}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {row.name}
-                                </TableCell>
-                                <TableCell align="right">
-                                    {row.selection}
-                                </TableCell>
-                                <TableCell align="right">
-                                    {row.tags}
-                                </TableCell>
-                                <TableCell align="right">
-                                    {row.vocabulary}
-                                </TableCell>
-                                <TableCell align="right">
-                                    {row.media}
-                                </TableCell>
-                                <TableCell align="right">
-                                    {row.owner}
-                                </TableCell>
-                                <TableCell align="right">
-                                    {row.date}
-                                </TableCell>
-                            </StyledTableRow>
-                        ))}
+                        {rows.slice().sort(getComparator(
+                            this.state.order, this.state.orderBy))
+                            .map((row, index) => {
+                                const labelId =
+                                `enhanced-table-checkbox-${index}`;
+                                return (
+                                    <StyledTableRow
+                                        tabIndex={-1}
+                                        key={index}
+                                    >
+                                        <TableCell
+                                            component="th"
+                                            id={labelId}
+                                            scope="row"
+                                            padding="none"
+                                        >
+                                            {row.title}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {row.selections}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {row.tags}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {row.vocabulary}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {row.media}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {row.owner}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {row.date}
+                                        </TableCell>
+                                    </StyledTableRow>
+                                );
+                            })
+                        }
                     </TableBody>
                 </Table>
             </TableContainer>

fb19e639e14979a7216595fa820eb33eb523a412
ndittren <36773036+ndittren@users.noreply.github.com>
2023-02-13 12:35:22 -0500

    skip this particular exception

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -20,3 +20,13 @@ import './commands';
 // require('./commands')
 
 import 'cypress-axe';
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // we expect a 3rd party library error with message 'list not defined'
+    // and don't want to fail the test so we return false
+    if (err.message.includes('u is not a constructor')) {
+        return false;
+    }
+    // we still want to ensure there are no other unexpected
+    // errors, so we let them fail the test
+});

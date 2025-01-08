2f637466c2d1b6add63cbe7db02b71f02169b804
nikolas <nikolas@gnu.org>
2025-01-08 11:15:01 -0500

    Remove prefetch_related on collaboration call
    In development mode, removing this prefetch increases both the SQL queries and CPU load, but for some reason allows the page to actually load in the staging environment. So, I want to deploy this to see if it changes anything in production.

--- structuredcollaboration/models.py
+++ structuredcollaboration/models.py
@@ -128,13 +128,7 @@ class Collaboration(models.Model):
         if author and not author.is_anonymous:
             queryset = queryset.filter(Q(user=author) | Q(group__user=author))
 
-        return queryset.select_related(
-            'user', 'group', 'policy_record'
-        ).prefetch_related(
-            'content_object',
-            'content_object__author',
-            'content_object__participants',
-            'content_object__collaboration')
+        return queryset.select_related('user', 'group', 'policy_record')
 
     def get_top_ancestor(self):  # i.e. domain
         result = self

96d11239725c2acf7766c74b7594437076903d95
Nikolas Nyby <nnyby@columbia.edu>
2025-01-16 13:08:58 -0500

    Introduce django-lti - INFRA-2583
    For LTI 1.3 compatibility.
    
    An "LTI registration" object can be created in the lti_tool section of
    Django admin. This enables the view to be linked to from Canvas.
    
    Here are some of the configuration options which must be filled in when
    creating an LTI registration within the Django admin:
    * Client ID: The ID of the Developer Key created in Canvas
    * Auth URL: `https://<canvas hostname>/api/lti/authorize_redirect`
    * Access token URL: `https://<canvas hostname>/login/oauth2/auth`
    * Keyset URL: `https://<canvas hostname>/api/lti/security/jwks`
    
    I am not 100% certain on the accuracy of all of these values yet. I am
    still investigating the integration steps. It seems many different
    integration methods are possible.

--- lti_auth/urls.py
+++ lti_auth/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from lti_auth.views import LTIConfigView, LTILandingPage, LTIRoutingView, \
     LTICourseEnableView
+from lti_tool.views import jwks, OIDCLoginInitView
 
 
 urlpatterns = [
@@ -10,4 +11,9 @@ urlpatterns = [
     path('landing/<slug:context>/',
          LTILandingPage.as_view(), {}, 'lti-landing-page'),
     path('', LTIRoutingView.as_view(), {}, 'lti-login'),
+
+    # django-lti
+    path('.well-known/jwks.json', jwks, name='jwks'),
+    path('init/<uuid:registration_uuid>/',
+         OIDCLoginInitView.as_view(), name='oidc_init'),
 ]

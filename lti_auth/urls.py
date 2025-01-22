da5009a4e829e82ef5da4646b34f3b178e7ea3c4
Nikolas Nyby <nnyby@columbia.edu>
2025-01-22 17:01:36 -0500

    Add LTI 1.3 JSON configuration endpoint at `/lti/config.json`
    In Canvas LMS, an LTI Developer Key can be created via Manual
    Entry, or by URL. This view provides the JSON necessary for URL
    configuration in Canvas.
    
    https://canvas.instructure.com/doc/api/file.lti_dev_key_config.html

--- lti_auth/urls.py
+++ lti_auth/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
-from lti_auth.views import LTIConfigView, LTILandingPage, LTIRoutingView, \
-    LTICourseEnableView
+from lti_auth.views import (
+    LTIConfigView, LTILandingPage, LTIRoutingView,
+    LTICourseEnableView, LTI1p3JSONConfigView
+)
 from lti_tool.views import jwks, OIDCLoginInitView
 
 
 urlpatterns = [
     path('config.xml', LTIConfigView.as_view(), {}, 'lti-config'),
+    path('config.json', LTI1p3JSONConfigView.as_view(), name='lti-1p3-config'),
     path('enable/', LTICourseEnableView.as_view(), {}, 'lti-enable-course'),
     path('landing/<slug:context>/',
          LTILandingPage.as_view(), {}, 'lti-landing-page'),

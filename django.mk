0541b821cd3d7ca4d146bb0e7422bf1b2315f172
Nikolas Nyby <nnyby@columbia.edu>
2025-10-23 10:54:13 -0400

    Update django.mk, remove coverage

--- django.mk
+++ django.mk
@@ -1,30 +1,32 @@
-# VERSION=1.9.0
+# VERSION=1.10.0
 
 # CHANGES:
-# 1.9.0              - Use coverage tool directly to generate coverage
-#                      reports.
-#                    - wheel and pip updates
-#                    - Use pre-compiled binary wheel for cryptography
-#                    - Remove Travis references
-# 1.8.0 - 2019-10-21 - Don't run flake8 on local_settings.py
-# 1.7.0 - 2018-05-31 - Now using python 3 by default
-#                    - Removed virtualenv.py in favor of python 3's
-#                      builtin venv capability.
-# 1.6.0 - 2017-09-05 - add bandit secure analysis configuration
-# 1.5.0 - 2017-08-24 - remove jshint/jscs in favor of eslint
-# 1.4.0 - 2017-06-06 - backout the switch to eslint. that's not really ready yet.
-# 1.3.0 - 2017-06-05 - pypi location is not needed anymore
-# 1.2.0 - 2016-12-15 - bump wheel version to 0.29
-# 1.1.0 - 2016-11-08 - run flake8 tests before unit tests
-# 1.0.1 - 2016-05-02 - Remove deprecated syncdb command from make install
+# 1.10.0 - 2025-10-16 - Removed coverage, updated dependencies.
+# 1.9.0               - Use coverage tool directly to generate coverage
+#                       reports.
+#                     - wheel and pip updates
+#                     - Use pre-compiled binary wheel for cryptography
+#                     - Remove Travis references
+# 1.8.0  - 2019-10-21 - Don't run flake8 on local_settings.py
+# 1.7.0  - 2018-05-31 - Now using python 3 by default
+#                     - Removed virtualenv.py in favor of python 3's
+#                       builtin venv capability.
+# 1.6.0  - 2017-09-05 - add bandit secure analysis configuration
+# 1.5.0  - 2017-08-24 - remove jshint/jscs in favor of eslint
+# 1.4.0  - 2017-06-06 - backout the switch to eslint. that's not really ready yet.
+# 1.3.0  - 2017-06-05 - pypi location is not needed anymore
+# 1.2.0  - 2016-12-15 - bump wheel version to 0.29
+# 1.1.0  - 2016-11-08 - run flake8 tests before unit tests
+# 1.0.1  - 2016-05-02 - Remove deprecated syncdb command from make install
 
 VE ?= ./ve
 MANAGE ?= ./manage.py
 REQUIREMENTS ?= requirements.txt
 SYS_PYTHON ?= python3
 PY_SENTINAL ?= $(VE)/sentinal
-WHEEL_VERSION ?= 0.44.0
-PIP_VERSION ?= 24.2
+WHEEL_VERSION ?= 0.45.1
+PIP_VERSION ?= 25.2
+SETUPTOOLS_VERSION ?= 80.9.0
 MAX_COMPLEXITY ?= 10
 INTERFACE ?= localhost
 RUNSERVER_PORT ?= 8000
@@ -32,7 +34,6 @@ PY_DIRS ?= $(APP)
 BANDIT ?= $(VE)/bin/bandit
 FLAKE8 ?= $(VE)/bin/flake8
 PIP ?= $(VE)/bin/pip
-COVERAGE ?= $(VE)/bin/coverage
 
 jenkins: check flake8 test bandit
 
@@ -40,14 +41,13 @@ $(PY_SENTINAL): $(REQUIREMENTS)
 	rm -rf $(VE)
 	$(SYS_PYTHON) -m venv $(VE)
 	$(PIP) install pip==$(PIP_VERSION)
-	$(PIP) install --upgrade setuptools
+	$(PIP) install setuptools==$(SETUPTOOLS_VERSION)
 	$(PIP) install wheel==$(WHEEL_VERSION)
 	$(PIP) install --no-deps --requirement $(REQUIREMENTS)
 	touch $@
 
 test: $(PY_SENTINAL)
-	$(COVERAGE) run --source='.' --omit=$(VE)/* $(MANAGE) test $(PY_DIRS)
-	$(COVERAGE) xml -o reports/coverage.xml
+	$(MANAGE) test $(PY_DIRS)
 
 parallel-tests: $(PY_SENTINAL)
 	$(MANAGE) test --parallel
@@ -75,7 +75,6 @@ clean:
 	rm -rf media/CACHE
 	rm -rf reports
 	rm -f celerybeat-schedule
-	rm -f .coverage
 	rm -rf node_modules
 	find . -name '*.pyc' -exec rm {} \;
 

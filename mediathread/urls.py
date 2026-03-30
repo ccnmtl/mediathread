64e5554a3cbd13ac5bfdb45be3423078c81b41fe
Nikolas Nyby <nnyby@columbia.edu>
2026-03-30 13:03:35 -0400

    django-debug-toolbar: don't import when disabled

--- mediathread/urls.py
+++ mediathread/urls.py
@@ -46,7 +46,6 @@ from mediathread.projects.views import (
 from mediathread.taxonomy.api import TermResource, VocabularyResource
 from registration.backends.default.views import RegistrationView
 from tastypie.api import Api
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 tastypie_api = Api('')
@@ -289,6 +288,8 @@ urlpatterns = [
 ]
 
 if settings.DEVELOPMENT:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+
     urlpatterns = [
         *urlpatterns,
     ] + debug_toolbar_urls()

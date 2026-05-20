d73e27acfa0b94309647a893794cd5ccfb433f5f
Nikolas Nyby <nnyby@columbia.edu>
2026-05-20 12:35:59 -0400

    Remove @babel/eslint-parser
    This package is not needed, and removing it makes eslint upgrades
    easier.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "eslint/config";
 import react from "eslint-plugin-react";
 import globals from "globals";
-import babelParser from "@babel/eslint-parser";
 import pluginCypress from 'eslint-plugin-cypress';
 
 export default defineConfig([
@@ -20,22 +19,21 @@ export default defineConfig([
         },
 
         languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-                ...globals.jquery,
-                Backbone: true,
-                _: true,
-            },
-
-            parser: babelParser,
-            ecmaVersion: 6,
-            sourceType: "module",
+            ecmaVersion: 'latest',
+            sourceType: 'module',
 
             parserOptions: {
                 ecmaFeatures: {
                     jsx: true,
                 },
+            },
+
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                ...globals.jquery,
+                Backbone: 'readonly',
+                _: 'readonly',
             },
         },
 
